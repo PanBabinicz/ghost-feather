c45a985575804d609f057b5398e90b586665be83
PanBabinicz <babinicz007@icloud.com>
2024-12-01 23:30:26 +0100

    WIP: Add dfu_usart.py script #3. Add dfu_updater class

--- scripts/dfu_usart.py
+++ scripts/dfu_usart.py
@@ -1,29 +1,39 @@
 import sys
 import serial
 
-def convert_to_little_endian(four_bytes_segment):
-    swapped = ''.join([four_bytes_segment[i:i+2] for i in range(0, len(four_bytes_segment), 2)][::-1])
-    print(swapped)
+class dfu_updater:
+    def __init__(self):
+        if ( len(sys.argv) < 4 ):
+            print("Invalid usage: python3 dfu_usart.py <bin> <port> <baudrate>")
+            sys.exit()
 
-if ( len(sys.argv) < 4 ):
-    print("Invalid usage: python3 dfu_usart.py <bin> <port> <baudrate>")
-    sys.exit()
+        self.bin      = sys.argv[1]
+        self.port     = sys.argv[2]
+        self.baudrate = sys.argv[3]
 
-bin      = sys.argv[1]
-port     = sys.argv[2]
-baudrate = sys.argv[3]
+    def print_arguments(self):
+        print("Bin:      " + self.bin)
+        print("Port:     " + self.port)
+        print("Baudrate: " + self.baudrate)
+
+    def four_byte_convert_to_little_endian(self, four_bytes_segment):
+        swapped = ''.join([four_bytes_segment[i:i+2] for i in range(0, len(four_bytes_segment), 2)][::-1])
+        print("0x" + swapped)
+
+    def segment_convert_to_little_endian(self, segment_start, segment_stop):
+        for i in range(2*segment_start, 2*segment_stop, 8):
+            chunk = hexdata[i:i+8]
+            updater.four_byte_convert_to_little_endian(chunk)
+
 # usart    = serial.Serial(port=port, baudrate=baudrate, timeout=None)
 
-print("Bin:      " + bin)
-print("Port:     " + port)
-print("Baudrate: " + baudrate)
+updater = dfu_updater()
+updater.print_arguments()
 
-with open(bin, 'rb') as bin_file:
+with open(updater.bin, 'rb') as bin_file:
     hexdata = bin_file.read().hex()
 
-print(hexdata[2*0x1000:2*0x1020])
-
-convert_to_little_endian(hexdata[2*0x01000:2*0x01004])
+updater.segment_convert_to_little_endian(0x1000, 0x1498)
 
 # byte = usart.read(size=1)
 # print(byte)
