fdd4ac2f10d8f97cd68dce595ac897f79d7f7e2e
PanBabinicz <babinicz007@icloud.com>
2025-01-14 20:43:57 +0100

    WIP: Add dfu_usart.py script #56. Unlock the FPU on Cortex M7

--- startup/stm32f722xx/first_bootloader/stm32722xx_first_bootloader_startup.c
+++ startup/stm32f722xx/first_bootloader/stm32722xx_first_bootloader_startup.c
@@ -1,5 +1,7 @@
 #include "stm32f722xx_first_bootloader.h"
 
+#define CPACR   (*(volatile uint32_t*)(0xe000ed88))
+
 extern uint32_t _sbss;
 extern uint32_t _ebss;
 extern uint32_t _sdata;
@@ -9,17 +11,28 @@ extern uint32_t _etext;
 extern void _reset_handler(void);
 
 ///
+/// \breif Unlock the FPU on Cortex M7.
+///
+static void fpu_unlock();
+
+static void fpu_unlock()
+{
+    /* CP11 and CP10 are (23-22) and (21-20) bits respectively. */
+    CPACR |= ((0x03 << 0x16) | (0x03 <<0x14));
+}
+
+///
 /// \breif This reset handler function is used to prepare for the first bootloader.
 ///
 void _reset_handler(void)
 {
-    /* Clear the bss section */
+    /* Clear the bss section. */
     for (uint32_t *bss = &_sbss; bss < &_ebss; )
     {
         *bss++ = 0;
     }
 
-    /* Set the global and static variables */
+    /* Set the global and static variables. */
     uint32_t *init_value = &_etext;
     uint32_t *data       = &_sdata;
 
@@ -30,6 +43,9 @@ void _reset_handler(void)
             *data++ = *init_value++;
         }
     }
+
+    /* Unlock the FPU. */
+    fpu_unlock();
 
     first_bootloader_start();
 
