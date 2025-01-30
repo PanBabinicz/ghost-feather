35303935a61167661a6afefaa4c63d9a4facf716
PanBabinicz <babinicz007@icloud.com>
2025-01-30 22:51:07 +0100

    feature: Add libopencm3 stm32 usart mock

---
+++ tests/gmock/libopencm3/stm32/usart.h
@@ -0,0 +1,48 @@
+#ifndef _GMOCK_LIBOPENCM3_USART_H
+#define _GMOCK_LIBOPENCM3_USART_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif  /* __cplusplus */
+
+///
+/// \breif Mock implementation of usart_wait_send_ready function.
+///
+static inline void usart_wait_send_ready(uint32_t usart)
+{
+    printf("GMOCK: %s(usart = %d)\n", __func__, usart);
+}
+
+///
+/// \breif Mock implementation of usart_wait_recv_ready function.
+///
+static inline void usart_wait_recv_ready(uint32_t usart)
+{
+    printf("GMOCK: %s(usart = %d\n", __func__, usart);
+}
+
+///
+/// \breif Mock implementation of usart_send function.
+///
+static inline void usart_send(uint32_t usart, uint16_t data)
+{
+    printf("GMOCK: %s(usart = %d, data = 0x%x)\n", __func__, usart, data);
+}
+
+///
+/// \breif Mock implementation of usart_recv function.
+///
+static inline uint16_t usart_recv(uint32_t usart)
+{
+    printf("GMOCK: %s(usart = %d\n", __func__, usart);
+    return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif  /* __cplusplus */
+
+#endif  /* _GMOCK_LIBOPENCM3_USART */
