bc1d6f7de21da345bd122d756634932acfe49d7f
PanBabinicz <babinicz007@icloud.com>
2025-02-06 23:22:41 +0100

    feature: Add the usart enable and disable mock functions

--- tests/gmock/libopencm3/stm32/usart.h
+++ tests/gmock/libopencm3/stm32/usart.h
@@ -4,9 +4,28 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define USART_CR1_UE    (1 << 0x00)
+
+#define USART_CR2_UE    (1 << 0x00)
+
 #ifdef __cplusplus
 extern "C" {
 #endif  /* __cplusplus */
+
+///
+/// \breif Mock definition of USART control register 1.
+///
+static inline uint32_t USART_CR1 = 0x00000000;
+
+///
+/// \breif Mock definition of USART control register 2.
+///
+static inline uint32_t USART_CR2 = 0x00000000;
+
+///
+/// \breif Mock definition of USART baud rate register.
+///
+static inline uint32_t USART_BRR = 0x00000000;
 
 ///
 /// \breif Mock implementation of usart_wait_send_ready function.
@@ -38,6 +57,30 @@ static inline void usart_send(uint32_t usart, uint16_t data)
 static inline uint16_t usart_recv(uint32_t usart)
 {
     return 1;
+}
+
+///
+/// \breif Mock implementation of usart_enable function.
+///
+static inline void usart_enable(uint32_t usart)
+{
+	USART_CR1 |= USART_CR1_UE;
+}
+
+///
+/// \breif Mock implementation of usart_disable function.
+///
+static inline void usart_disable(uint32_t usart)
+{
+	USART_CR1 &= ~USART_CR1_UE;
+}
+
+///
+/// \breif Mock implementation of usart_set_baudrate function.
+///
+static inline void usart_set_baudrate(uint32_t usart, uint32_t baud)
+{
+
 }
 
 #ifdef __cplusplus
