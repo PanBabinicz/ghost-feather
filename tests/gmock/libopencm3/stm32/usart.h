763cb390db52f01c245b49350132921d854905ad
PanBabinicz <babinicz007@icloud.com>
2025-02-07 23:30:20 +0100

    feature: Add more usart mock functions

--- tests/gmock/libopencm3/stm32/usart.h
+++ tests/gmock/libopencm3/stm32/usart.h
@@ -4,9 +4,24 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define USART_CR1_UE    (1 << 0x00)
+#define USART3              (0x00000000)
 
-#define USART_CR2_UE    (1 << 0x00)
+#define USART_CR1_M0        (1 << 0x0c)
+#define USART_CR1_PCE       (1 << 0x0a)
+#define USART_CR1_PS        (1 << 0x09)
+#define USART_CR1_TE        (1 << 0x03)
+#define USART_CR1_RE        (1 << 0x02)
+#define USART_CR1_UE        (1 << 0x00)
+
+#define USART_PARITY_NONE   (0x00)
+#define USART_PARITY_EVEN   (USART_CR1_PCE)
+#define USART_PARITY_ODD    (USART_CR1_PS | USART_CR1_PCE)
+#define USART_PARITY_MASK   (USART_CR1_PS | USART_CR1_PCE)
+
+#define USART_MODE_RX       (USART_CR1_RE)
+#define USART_MODE_TX       (USART_CR1_TE)
+#define USART_MODE_TX_RX    (USART_CR1_RE | USART_CR1_TE)
+#define USART_MODE_MASK     (USART_CR1_RE | USART_CR1_TE)
 
 #ifdef __cplusplus
 extern "C" {
@@ -15,17 +30,17 @@ extern "C" {
 ///
 /// \breif Mock definition of USART control register 1.
 ///
-static inline uint32_t USART_CR1 = 0x00000000;
+static uint32_t USART_CR1 = 0x00000000;
 
 ///
 /// \breif Mock definition of USART control register 2.
 ///
-static inline uint32_t USART_CR2 = 0x00000000;
+static uint32_t USART_CR2 = 0x00000000;
 
 ///
 /// \breif Mock definition of USART baud rate register.
 ///
-static inline uint32_t USART_BRR = 0x00000000;
+static uint32_t USART_BRR = 0x00000000;
 
 ///
 /// \breif Mock implementation of usart_wait_send_ready function.
@@ -80,7 +95,52 @@ static inline void usart_disable(uint32_t usart)
 ///
 static inline void usart_set_baudrate(uint32_t usart, uint32_t baud)
 {
+    /* Lets say that we use HSI as sysclk. */
+	uint32_t clock = 16000000U;
 
+	USART_BRR = (clock + baud / 2) / baud;
+}
+
+///
+/// \breif Mock implementation of usart_set_databits function.
+///
+static inline void usart_set_databits(uint32_t usart, uint32_t bits)
+{
+	if (bits == 8)
+    {
+        /* 8 data bits. */
+		USART_CR1 &= ~USART_CR1_M0;
+	}
+    else
+    {
+        /* 9 data bits. */
+		USART_CR1 |= USART_CR1_M0;
+	}
+}
+
+///
+/// \breif Mock implementation of usart_set_parity function.
+///
+static inline void usart_set_parity(uint32_t usart, uint32_t parity)
+{
+	uint32_t reg32;
+
+	reg32 = USART_CR1;
+	reg32 = (reg32 & ~USART_PARITY_MASK) | parity;
+	USART_CR1 = reg32;
+}
+
+
+///
+/// \breif Mock implementation of usart_set_mode function.
+///
+static inline void usart_set_mode(uint32_t usart, uint32_t mode)
+{
+	uint32_t reg32;
+
+	reg32 = USART_CR1;
+	reg32 = (reg32 & ~USART_MODE_MASK) | mode;
+	USART_CR1 = reg32;
 }
 
 #ifdef __cplusplus
