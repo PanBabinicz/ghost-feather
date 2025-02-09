cc79f506e9a6a9758afa30c45d9b2c514617199e
PanBabinicz <babinicz007@icloud.com>
2025-02-09 16:40:11 +0100

    feature: Add usart controller status check inside init unit test

--- tests/gtest/controller/usart/init/init.cc
+++ tests/gtest/controller/usart/init/init.cc
@@ -13,13 +13,16 @@ uint32_t USART_BRR = 0x00000000;
 ///
 TEST(init, procedure)
 {
+    usart_controller_status_t status;
     uint32_t expected_usart_cr1 = 0x0000000d;
 
     /* Using SYSCLK because of the default value of AHB and APB buses prescalers. */
     uint32_t expected_usart_brr = ((SYSCLK + 115200 / 2 ) / 115200);
 
     usart_controller_debug_init();
+    status = usart_controller_is_initialized();
 
+    EXPECT_EQ(status, USART_CONTROLLER_STATUS_INIT);
     EXPECT_EQ(USART_CR1, expected_usart_cr1);
     EXPECT_EQ(USART_BRR, expected_usart_brr);
 }
