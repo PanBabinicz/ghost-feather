b32c2b3baf9791c1b47efbe3f9acad31d13f6de5
PanBabinicz <babinicz007@icloud.com>
2025-02-10 22:29:22 +0100

    feature: Add circular buffer clear functions unit tests

---
+++ tests/gtest/data_structure/circular_buffer/clear/clear.cc
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include <stdint.h>
+#include "data_structure/circular_buffer.h"
+
+///
+/// \brief This test performs the single circular buffer instance clear procedure.
+///
+TEST(clear, single)
+{
+    circular_buffer_result_t result;
+
+    result = circular_buffer_clear(CIRCULAR_BUFFER_INSTANCE_0);
+    EXPECT_EQ(result, CIRCULAR_BUFFER_RESULT_SUCCESS);
+}
+
+///
+/// \brief This test performs all circular buffer instances clear procedure.
+///
+TEST(clear, all)
+{
+    circular_buffer_result_t result;
+
+    result = circular_buffer_clear_all();
+    EXPECT_EQ(result, CIRCULAR_BUFFER_RESULT_SUCCESS);
+}
