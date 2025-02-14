4819468e5699b4b1e8f024e74199c9b12b77419e
PanBabinicz <babinicz007@icloud.com>
2025-02-14 21:03:53 +0100

    feature: Add circular buffer pop overflow, null protection and invalid instance unit tests

--- tests/gtest/data_structure/circular_buffer/pop/pop.cc
+++ tests/gtest/data_structure/circular_buffer/pop/pop.cc
@@ -36,6 +36,22 @@ class pop : public ::testing::Test
                 /* rear = 0; front = 0; overflow = 1; */
                 (void)circular_buffer_set_state(CIRCULAR_BUFFER_INSTANCE_0, 0, 0, 1);
             }
+            else if (::testing::UnitTest::GetInstance()->current_test_info()->name() ==
+                     std::string("overflow"))
+            {
+                uint8_t buffer[CIRCULAR_BUFFER_LENGTH];
+
+                for (size_t i = 0; i < CIRCULAR_BUFFER_LENGTH; i++)
+                {
+                    buffer[i] = i;
+                }
+
+                (void)circular_buffer_set_data(CIRCULAR_BUFFER_INSTANCE_0, &buffer[0],
+                                               CIRCULAR_BUFFER_LENGTH);
+
+                /* rear = 4; front = 3; overflow = 1; */
+                (void)circular_buffer_set_state(CIRCULAR_BUFFER_INSTANCE_0, 4, 3, 1);
+            }
         }
 };
 
@@ -82,4 +98,45 @@ TEST_F(pop, empty)
     EXPECT_EQ(result, CIRCULAR_BUFFER_RESULT_EMPTY);
 
     EXPECT_EQ(element, 0xff);
+}
+
+///
+/// \brief This test pops element from overflowed circular buffer.
+///
+TEST_F(pop, overflow)
+{
+    circular_buffer_result_t result;
+    uint8_t element;
+
+    for (size_t i = 0; i < CIRCULAR_BUFFER_LENGTH; i++)
+    {
+        result = circular_buffer_pop(CIRCULAR_BUFFER_INSTANCE_0, &element);
+        EXPECT_EQ(result, CIRCULAR_BUFFER_RESULT_SUCCESS);
+
+        /* rear starts from fifth element in this scenario. */
+        EXPECT_EQ(element, (i + 4) % CIRCULAR_BUFFER_LENGTH);
+    }
+}
+
+///
+/// \brief This test checks the null pointer protection inside pop function.
+///
+TEST_F(pop, null_pointer_protection)
+{
+    circular_buffer_result_t result;
+
+    result = circular_buffer_pop(CIRCULAR_BUFFER_INSTANCE_0, NULL);
+    EXPECT_EQ(result, CIRCULAR_BUFFER_RESULT_NULL_POINTER);
+}
+
+///
+/// \brief This test checks the invalid instance protection inside pop function.
+///
+TEST_F(pop, invalid_instance)
+{
+    circular_buffer_result_t result;
+    uint8_t element;
+
+    result = circular_buffer_pop((circular_buffer_instance_t)0xff, &element);
+    EXPECT_EQ(result, CIRCULAR_BUFFER_RESULT_INVALID_INSTANCE);
 }
