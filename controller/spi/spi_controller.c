320b5afd4ae63ef9ea9d3e28a0872cbb36a8f4a3
PanBabinicz <babinicz007@icloud.com>
2025-06-12 10:57:48 +0200

    feat: Add spi controller init/deinit functions #2. Add clock polarity and phase setters

--- controller/spi/spi_controller.c
+++ controller/spi/spi_controller.c
@@ -10,8 +10,10 @@
 ///
 typedef struct spi_controller
 {
-    uint32_t spi                                    /*!< The spi peripheral identifier.                     */
-    bool     is_init;                               /*!< The is initialized flag.                           */
+    uint32_t                        interface       /*!< The spi peripheral interface.                      */
+    spi_controller_clock_phase_t    clock_phase     /*!< The clock phase index.                             */
+    spi_controller_clock_polarity_t clock_polarity  /*!< The clock polarity index.                          */
+    bool                            is_init;        /*!< The is initialized flag.                           */
 } spi_controller_t;
 
 ///***********************************************************************************************************
@@ -19,13 +21,37 @@ typedef struct spi_controller
 ///***********************************************************************************************************
 static spi_controller_t spi_controller =
 {
-    .spi     = SPI1,
-    .is_init = false,
+    .interface      = SPI1,
+    .clock_phase    = SPI_CONTROLLER_CLOCK_PHASE_0,
+    .clock_polarity = SPI_CONTROLLER_CLOCK_POLARITY_1,
+    .is_init        = false,
+};
+
+///
+/// \brief Contains function pointers that allow the clock phase to be set using the libopencm3 functions.
+///
+static void (*const spi_controller_set_clock_phase_array[SPI_CONTROLLER_CLOCK_PHASE_TOTAL])(uint32_t interface) =
+{
+    spi_set_clock_phase_0,
+    spi_set_clock_phase_1
+};
+
+///
+/// \brief Contains function pointers that allow the clock polarity to be set using the libopencm3 functions.
+///
+static void (*const spi_controller_set_clock_polarity_array[SPI_CONTROLLER_CLOCK_POLARITY_TOTAL])(uint32_t interface) =
+{
+    spi_set_clock_polarity_0,
+    spi_set_clock_polarity_1
 };
 
 ///***********************************************************************************************************
 /// Private functions - declaration.
 ///***********************************************************************************************************
+///
+/// \brief
+///
+static spi_controller_result_t spi_controller
 
 ///***********************************************************************************************************
 /// Private functions - definition.
@@ -41,7 +67,11 @@ spi_controller_result_t spi_controller_init(spi_controller_t *const spi_controll
         return SPI_CONTROLLER_RESULT_ERROR;
     }
 
-    spi_enable(spi_controller->spi);
+    spi_disable(spi_controller->interface);
+    spi_controller_set_clock_phase_array[spi_controller->clock_phase](spi_controller->interface);
+    spi_controller_set_clock_polarity_array[spi_controller->clock_polarity](spi_controller->interface);
+    spi_enable(spi_controller->interface);
+
     spi_controller->is_init = true;
 
     return SPI_CONTROLLER_RESULT_SUCCESS;
@@ -58,4 +88,43 @@ spi_controller_result_t spi_controller_deinit(spi_controller_t *const spi_contro
     spi_controller->is_init = false;
 
     return SPI_CONTROLLER_RESULT_SUCCESS;
+}
+
+spi_controller_result_t spi_controller_get_instance(const spi_controller_t **instance)
+{
+    if (instance == NULL)
+    {
+        return SPI_CONTROLLER_RESULT_ERROR;
+    }
+
+    *instance = &spi_controller;
+    return SPI_CONTROLLER_RESULT_SUCCESS;
+}
+
+spi_controller_result_t spi_controller_set_clock_phase(spi_controller_t *const instance,
+                                                       const spi_controller_clock_phase_t clock_phase)
+{
+    if ((clock_phase < SPI_CONTROLLER_CLOCK_PHASE_BEGIN) || (clock_phase >= SPI_CONTROLLER_CLOCK_PHASE_TOTAL)
+        (instance == NULL))
+    {
+        return SPI_CONTROLLER_RESULT_ERROR;
+    }
+
+    instance->clock_phase = clock_phase;
+
+    return SPI_CONTROLLER_RESULT_ERROR;
+}
+
+spi_controller_result_t spi_controller_set_clock_polarity(spi_controller_t *const instance,
+                                                          const spi_controller_clock_polarity_t clock_polarity)
+{
+    if ((clock_polarity < SPI_CONTROLLER_CLOCK_POLARITY_BEGIN) || (clock_polarity >= SPI_CONTROLLER_CLOCK_POLARITY_TOTAL)
+        (instance == NULL))
+    {
+        return SPI_CONTROLLER_RESULT_ERROR;
+    }
+
+    instance->clock_polarity = clock_polarity;
+
+    return SPI_CONTROLLER_RESULT_ERROR;
 }
--- controller/spi/spi_controller.h
+++ controller/spi/spi_controller.h
@@ -18,6 +18,28 @@ typedef enum spi_controller_result
 } spi_controller_result_t;
 
 ///
+/// \brief The spi controller clock phase type.
+///
+typedef enum spi_controller_clock_phase
+{
+    SPI_CONTROLLER_CLOCK_PHASE_BEGIN = 0,
+    SPI_CONTROLLER_CLOCK_PHASE_0     = 0,
+    SPI_CONTROLLER_CLOCK_PHASE_1,
+    SPI_CONTROLLER_CLOCK_PHASE_TOTAL,
+} spi_controller_clock_phase_t;
+
+///
+/// \brief The spi controller clock polarity type.
+///
+typedef enum spi_controller_clock_polarity
+{
+    SPI_CONTROLLER_CLOCK_POLARITY_BEGIN = 0,
+    SPI_CONTROLLER_CLOCK_POLARITY_0     = 0,
+    SPI_CONTROLLER_CLOCK_POLARITY_1,
+    SPI_CONTROLLER_CLOCK_POLARITY_TOTAL,
+} spi_controller_clock_polarity_t;
+
+///
 /// \brief Initializes the spi controller.
 ///
 /// \param[in] spi_controller             The spi controller instance.
@@ -38,5 +60,42 @@ spi_controller_result_t spi_controller_init(spi_controller_t *const spi_controll
 /// \retval SPI_CONTROLLER_RESULT_SUCCESS Otherwise.
 ///
 spi_controller_result_t spi_controller_deinit(spi_controller_t *const spi_controller);
+
+///
+/// \brief Gets the spi instance.
+///
+/// \param[out] instance                  The pointer to spi controller instance.
+///
+/// \return spi_controller_result_t       The spi controller result.
+/// \retval SPI_CONTROLLER_RESULT_SUCCESS On success.
+/// \retval SPI_CONTROLLER_RESULT_SUCCESS Otherwise.
+///
+spi_controller_result_t spi_controller_get_instance(const spi_controller_t **instance);
+
+///
+/// \brief Sets the spi clock phase index inside instance.
+///
+/// \param[in] spi_controller_clock_phase The spi controller instance.
+/// \param[in] spi_controller_clock_phase The spi controller clock phase index.
+///
+/// \return spi_controller_result_t       The spi controller result.
+/// \retval SPI_CONTROLLER_RESULT_SUCCESS On success.
+/// \retval SPI_CONTROLLER_RESULT_SUCCESS Otherwise.
+///
+spi_controller_result_t spi_controller_set_clock_phase(spi_controller_t *const instance,
+                                                       const spi_controller_clock_phase_t clock_phase);
+
+///
+/// \brief Sets the spi clock polarity index inside instance.
+///
+/// \param[in] instance                      The spi controller instance.
+/// \param[in] spi_controller_clock_polarity The spi controller clock polarity index.
+///
+/// \return spi_controller_result_t          The spi controller result.
+/// \retval SPI_CONTROLLER_RESULT_SUCCESS    On success.
+/// \retval SPI_CONTROLLER_RESULT_SUCCESS    Otherwise.
+///
+spi_controller_result_t spi_controller_set_clock_polarity(spi_controller_t *const instance,
+                                                          const spi_controller_clock_polarity_t clock_polarity);
 
 #endif  /* _SPI_CONTROLLER_H */
