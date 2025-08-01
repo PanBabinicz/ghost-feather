d9dc75cc3a5d97b57e1cb6d36100772f78aa9ffd
PanBabinicz <babinicz007@icloud.com>
2025-08-01 21:10:43 +0200

    feat: Add bmi270 time_get prcedure and null pointer protection unit tests

---
+++ tests/gtest/controller/bmi270/time_get/time_get.cc
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+#include <stdint.h>
+#include "bmi270.h"
+
+class gtest_bmi270_time_get : public ::testing::Test
+{
+    protected:
+        void SetUp() override
+        {
+            if (::testing::UnitTest::GetInstance()->current_test_info()->name() ==
+                std::string("procedure"))
+            {
+                struct bmi270_dev *dev = bmi270_get_dev();
+                uint32_t time = 0xdeadbeef;
+
+                (void)bmi270_time_set(dev, time);
+            }
+        }
+};
+
+TEST_F(gtest_bmi270_time_get, procedure)
+{
+    bmi270_res_t res;
+    uint32_t time;
+
+    struct bmi270_dev *dev = NULL;
+    dev = bmi270_get_dev();
+
+    res = bmi270_time_get(dev, &time);
+    EXPECT_EQ(res, BMI270_RES_OK);
+    /* TODO: Check the tiem value. */
+    //EXPECT_EQ(time, 0xdeadbeef);
+}
+
+///
+/// \brief This test checks the null pointer protection inside bmi270 deinit function.
+///
+TEST_F(gtest_bmi270_time_get, null_pointer_protection)
+{
+    bmi270_res_t res;
+    uint32_t time;
+
+    struct bmi270_dev *dev = NULL;
+    dev = bmi270_get_dev();
+
+    res = bmi270_time_get(NULL, &time);
+    EXPECT_EQ(res, BMI270_RES_ERR);
+
+    res = bmi270_time_get(dev, NULL);
+    EXPECT_EQ(res, BMI270_RES_ERR);
+}
