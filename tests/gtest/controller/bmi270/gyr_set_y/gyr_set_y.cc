eff65c81640ee9eaaea29ec4cf355248604961c5
PanBabinicz <babinicz007@icloud.com>
2025-08-05 23:29:15 +0200

    feat: Add bmi270 gyr_set_y procedure and null pointer protection unit tests

---
+++ tests/gtest/controller/bmi270/gyr_set_y/gyr_set_y.cc
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+#include <stdint.h>
+#include "bmi270.h"
+
+///
+/// \brief This test performs the bmi270 gyroscope set y axis procedure.
+///
+TEST(gtest_bmi270_gyr_set_y, procedure)
+{
+    bmi270_res_t res;
+    int16_t y;
+
+    struct bmi270_dev *dev = NULL;
+    dev = bmi270_get_dev();
+
+    res = bmi270_gyr_set_y(dev, 0xf00d);
+    EXPECT_EQ(res, BMI270_RES_OK);
+
+    res = bmi270_gyr_get_y(dev, &y);
+    EXPECT_EQ(res, BMI270_RES_OK);
+
+    EXPECT_EQ(y, (int16_t)0xf00d);
+}
+
+///
+/// \brief This test checks the null pointer protection inside bmi270 gyroscope
+///        set y function.
+///
+TEST(gtest_bmi270_gyr_set_y, null_pointer_protection)
+{
+    bmi270_res_t res;
+
+    struct bmi270_dev *dev = NULL;
+    dev = bmi270_get_dev();
+
+    res = bmi270_gyr_set_y(NULL, 0xf00d);
+    EXPECT_EQ(res, BMI270_RES_ERR);
+}
