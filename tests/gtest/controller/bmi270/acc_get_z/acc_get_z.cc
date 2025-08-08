ab6f50461cdc9d01ecef86ccc34c14c3c110adb8
PanBabinicz <babinicz007@icloud.com>
2025-08-08 20:44:57 +0200

    feat: Add bmi270 acc_get_z procedure and null pointer protection unit tests. Previous commit adds set function

---
+++ tests/gtest/controller/bmi270/acc_get_z/acc_get_z.cc
@@ -0,0 +1,55 @@
+#include <gtest/gtest.h>
+#include <stdint.h>
+#include "bmi270.h"
+
+class gtest_bmi270_acc_get_z : public ::testing::Test
+{
+    protected:
+        void SetUp() override
+        {
+            if (::testing::UnitTest::GetInstance()->current_test_info()->name() ==
+                std::string("procedure"))
+            {
+                struct bmi270_dev *dev = bmi270_get_dev();
+                int16_t z = 0xc0de;
+
+                (void)bmi270_acc_set_z(dev, z);
+            }
+        }
+};
+
+///
+/// \brief This test performs the bmi270 accelerometer get z axis procedure.
+///
+TEST_F(gtest_bmi270_acc_get_z, procedure)
+{
+    bmi270_res_t res;
+    int16_t z;
+
+    struct bmi270_dev *dev = NULL;
+    dev = bmi270_get_dev();
+
+    res = bmi270_acc_get_z(dev, &z);
+    EXPECT_EQ(res, BMI270_RES_OK);
+
+    EXPECT_EQ(z, (int16_t)0xc0de);
+}
+
+///
+/// \brief This test checks the null pointer protection inside bmi270 accelerometer
+///        get z function.
+///
+TEST_F(gtest_bmi270_acc_get_z, null_pointer_protection)
+{
+    bmi270_res_t res;
+    int16_t z;
+
+    struct bmi270_dev *dev = NULL;
+    dev = bmi270_get_dev();
+
+    res = bmi270_acc_get_z(NULL, &z);
+    EXPECT_EQ(res, BMI270_RES_ERR);
+
+    res = bmi270_acc_get_z(dev, NULL);
+    EXPECT_EQ(res, BMI270_RES_ERR);
+}
