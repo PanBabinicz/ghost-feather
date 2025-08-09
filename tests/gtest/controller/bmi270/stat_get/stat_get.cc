d6bccde41d79248b2dcb6a92e1ac19320d5d18c7
PanBabinicz <babinicz007@icloud.com>
2025-08-09 18:00:20 +0200

    feat: Add bmi270 stat_get procedure and null pointer protection unit tests

---
+++ tests/gtest/controller/bmi270/stat_get/stat_get.cc
@@ -0,0 +1,55 @@
+#include <gtest/gtest.h>
+#include <stdint.h>
+#include "bmi270.h"
+
+class gtest_bmi270_stat_get : public ::testing::Test
+{
+    protected:
+        void SetUp() override
+        {
+            if (::testing::UnitTest::GetInstance()->current_test_info()->name() ==
+                std::string("procedure"))
+            {
+                struct bmi270_dev *dev = bmi270_get_dev();
+                bmi270_stat_t stat = BMI270_STAT_INIT;
+
+                (void)bmi270_stat_set(dev, stat);
+            }
+        }
+};
+
+///
+/// \brief This test performs the bmi270 status get procedure.
+///
+TEST_F(gtest_bmi270_stat_get, procedure)
+{
+    bmi270_res_t res;
+    bmi270_stat_t stat;
+
+    struct bmi270_dev *dev = NULL;
+    dev = bmi270_get_dev();
+
+    res = bmi270_stat_get(dev, &stat);
+    EXPECT_EQ(res, BMI270_RES_OK);
+
+    EXPECT_EQ(stat, BMI270_STAT_INIT);
+}
+
+///
+/// \brief This test checks the null pointer protection inside bmi270 status
+///        get function.
+///
+TEST_F(gtest_bmi270_stat_get, null_pointer_protection)
+{
+    bmi270_res_t res;
+    bmi270_stat_t stat;
+
+    struct bmi270_dev *dev = NULL;
+    dev = bmi270_get_dev();
+
+    res = bmi270_stat_get(NULL, &stat);
+    EXPECT_EQ(res, BMI270_RES_ERR);
+
+    res = bmi270_stat_get(dev, NULL);
+    EXPECT_EQ(res, BMI270_RES_ERR);
+}
