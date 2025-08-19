f956fc6031528873f59804f3915c58cd619b0a8c
km1t4h <km1t4h@gmail.com>
2025-08-19 23:09:50 +0200

    feat: Add bmi270 temp_read procedure unit test

--- tests/gtest/controller/bmi270/temp_read/temp_read.cc
+++ tests/gtest/controller/bmi270/temp_read/temp_read.cc
@@ -7,17 +7,52 @@ uint32_t      SPI_CR1_ARR[SPI_INTF_TOTAL];
 uint32_t      SPI_CRCPR_ARR[SPI_INTF_TOTAL];
 struct spi_dr SPI_DR_ARR[SPI_INTF_TOTAL];
 
+class gtest_bmi270_temp_read : public ::testing::Test
+{
+    protected:
+        void SetUp() override
+        {
+            if (::testing::UnitTest::GetInstance()->current_test_info()->name() ==
+                std::string("procedure"))
+            {
+                /* It is not important what data goes inside fifo after that.
+                 * For example bmi270_acc_read function will push register address
+                 * to fifo during transmission. The fifo has two indexes, rx and tx.
+                 * It always starts reading from rx_idx which is 0. */
+                SPI_DR_ARR[SPI1].buf[0] = 0x00;
+                SPI_DR_ARR[SPI1].buf[1] = 0xad;
+                SPI_DR_ARR[SPI1].buf[2] = 0xde;
+            }
+        }
+};
+
 ///
 /// \brief This test performs the bmi270 temp read procedure.
 ///
-TEST(gtest_bmi270_temp_read, procedure)
+TEST_F(gtest_bmi270_temp_read, procedure)
 {
+    bmi270_res_t res;
+    /* TODO: Check the variable type in struct. */
+    int16_t temp;
+
+    struct bmi270_dev *dev = NULL;
+    dev = bmi270_get_dev();
+
+    res = bmi270_spi_ctrl_asg(dev);
+    EXPECT_EQ(res, BMI270_RES_OK);
+
+    res = bmi270_temp_read(dev);
+    EXPECT_EQ(res, BMI270_RES_OK);
+
+    res = bmi270_temp_get(dev, &temp);
+    EXPECT_EQ(res, BMI270_RES_OK);
+    EXPECT_EQ(x, (int16_t)(0xdead));
 }
 
 ///
 /// \brief This test checks the null pointer protection inside bmi270 temp read function.
 ///
-TEST(gtest_bmi270_temp_read, null_pointer_protection)
+TEST_F(gtest_bmi270_temp_read, null_pointer_protection)
 {
     bmi270_res_t res;
 
