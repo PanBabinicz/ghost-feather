9b686b916c0bc3ab5aa711684ea3ec3fdfb03e3e
km1t4h <km1t4h@gmail.com>
2025-08-20 17:27:16 +0200

    feat: Add bmi270 time_read procedure unit test

--- tests/gtest/controller/bmi270/time_read/time_read.cc
+++ tests/gtest/controller/bmi270/time_read/time_read.cc
@@ -7,17 +7,52 @@ uint32_t      SPI_CR1_ARR[SPI_INTF_TOTAL];
 uint32_t      SPI_CRCPR_ARR[SPI_INTF_TOTAL];
 struct spi_dr SPI_DR_ARR[SPI_INTF_TOTAL];
 
+class gtest_bmi270_time_read : public ::testing::Test
+{
+    protected:
+        void SetUp() override
+        {
+            if (::testing::UnitTest::GetInstance()->current_test_info()->name() ==
+                std::string("procedure"))
+            {
+                /* It is not important what data goes inside fifo after that.
+                 * For example bmi270_acc_read function will push register address
+                 * to fifo during transmission. The fifo has two indexes, rx and tx.
+                 * It always starts reading from rx_idx which is 0. */
+                SPI_DR_ARR[SPI1].buf[0] = 0x00;
+                SPI_DR_ARR[SPI1].buf[1] = 0xb0;
+                SPI_DR_ARR[SPI1].buf[2] = 0x0b;
+                SPI_DR_ARR[SPI1].buf[3] = 0xed;
+            }
+        }
+};
+
 ///
 /// \brief This test performs the bmi270 time read procedure.
 ///
-TEST(gtest_bmi270_time_read, procedure)
+TEST_F(gtest_bmi270_time_read, procedure)
 {
+    bmi270_res_t res;
+    uint32_t time;
+
+    struct bmi270_dev *dev = NULL;
+    dev = bmi270_get_dev();
+
+    res = bmi270_spi_ctrl_asg(dev);
+    EXPECT_EQ(res, BMI270_RES_OK);
+
+    res = bmi270_time_read(dev);
+    EXPECT_EQ(res, BMI270_RES_OK);
+
+    res = bmi270_time_get(dev, &time);
+    EXPECT_EQ(res, BMI270_RES_OK);
+    EXPECT_EQ(time, 0xb00bed);
 }
 
 ///
 /// \brief This test checks the null pointer protection inside bmi270 time read function.
 ///
-TEST(gtest_bmi270_time_read, null_pointer_protection)
+TEST_F(gtest_bmi270_time_read, null_pointer_protection)
 {
     bmi270_res_t res;
 
