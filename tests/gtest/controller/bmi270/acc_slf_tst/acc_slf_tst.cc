0cf766427448b55a149c6d667282b646838cc748
km1t4h <km1t4h@gmail.com>
2025-08-29 21:21:52 +0200

    feat: Improve resources sharing inside bmi270_acc_slf_tst test fixture class

--- tests/gtest/controller/bmi270/acc_slf_tst/acc_slf_tst.cc
+++ tests/gtest/controller/bmi270/acc_slf_tst/acc_slf_tst.cc
@@ -8,6 +8,35 @@ uint32_t      SPI_CRCPR_ARR[SPI_INTF_TOTAL];
 struct spi_dr SPI_DR_ARR[SPI_INTF_TOTAL];
 
 ///
+/// \brief The gtest_bmi270_acc_slf_tst test fixture class.
+///
+class gtest_bmi270_acc_slf_tst : public ::testing::Test
+{
+    protected:
+        static void SetUpTestSuite()
+        {
+            dev = bmi270_dev_get();
+        }
+
+        static void TearDownTestSuite()
+        {
+            dev = nullptr;
+        }
+
+        void SetUp() override
+        {
+        }
+
+        void TearDown() override
+        {
+        }
+
+        static bmi270_dev *dev;
+};
+
+struct bmi270_dev *gtest_bmi270_acc_slf_tst::dev = nullptr;
+
+///
 /// \brief This test performs the bmi270 accelerometer self test procedure.
 ///
 TEST(gtest_bmi270_acc_slf_tst, procedure)
