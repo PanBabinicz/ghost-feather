ab041f37d973aa360ab3f36deb5b75d388ab6df8
km1t4h <km1t4h@gmail.com>
2025-08-29 21:21:20 +0200

    feat: Improve resources sharing inside bmi270_acc_set_y test fixture class

--- tests/gtest/controller/bmi270/acc_set_y/acc_set_y.cc
+++ tests/gtest/controller/bmi270/acc_set_y/acc_set_y.cc
@@ -8,20 +8,46 @@ uint32_t      SPI_CRCPR_ARR[SPI_INTF_TOTAL];
 struct spi_dr SPI_DR_ARR[SPI_INTF_TOTAL];
 
 ///
+/// \brief The gtest_bmi270_acc_set_y test fixture class.
+///
+class gtest_bmi270_acc_set_y : public ::testing::Test
+{
+    protected:
+        static void SetUpTestSuite()
+        {
+            dev = bmi270_dev_get();
+        }
+
+        static void TearDownTestSuite()
+        {
+            dev = nullptr;
+        }
+
+        void SetUp() override
+        {
+        }
+
+        void TearDown() override
+        {
+        }
+
+        static bmi270_dev *dev;
+};
+
+struct bmi270_dev *gtest_bmi270_acc_set_y::dev = nullptr;
+
+///
 /// \brief This test performs the bmi270 accelerometer set y axis procedure.
 ///
-TEST(gtest_bmi270_acc_set_y, procedure)
+TEST_F(gtest_bmi270_acc_set_y, procedure)
 {
     bmi270_res_t res;
     int16_t y;
 
-    struct bmi270_dev *dev = NULL;
-    dev = bmi270_get_dev();
-
-    res = bmi270_acc_set_y(dev, 0xdabb);
+    res = bmi270_acc_set_y(gtest_bmi270_acc_set_y::dev, 0xdabb);
     EXPECT_EQ(res, BMI270_RES_OK);
 
-    res = bmi270_acc_get_y(dev, &y);
+    res = bmi270_acc_get_y(gtest_bmi270_acc_set_y::dev, &y);
     EXPECT_EQ(res, BMI270_RES_OK);
 
     EXPECT_EQ(y, (int16_t)0xdabb);
@@ -31,12 +57,9 @@ TEST(gtest_bmi270_acc_set_y, procedure)
 /// \brief This test checks the null pointer protection inside bmi270 accelerometer
 ///        set y function.
 ///
-TEST(gtest_bmi270_acc_set_y, null_pointer_protection)
+TEST_F(gtest_bmi270_acc_set_y, null_pointer_protection)
 {
     bmi270_res_t res;
-
-    struct bmi270_dev *dev = NULL;
-    dev = bmi270_get_dev();
 
     res = bmi270_acc_set_y(NULL, 0xdabb);
     EXPECT_EQ(res, BMI270_RES_ERR);
