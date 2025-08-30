d01ef60b386ad485985d90bde8d93239b3444dfd
km1t4h <km1t4h@gmail.com>
2025-08-30 20:22:39 +0200

    feat: Improve resources sharing inside bmi270_gyr_read test fixture class

--- tests/gtest/controller/bmi270/gyr_read/gyr_read.cc
+++ tests/gtest/controller/bmi270/gyr_read/gyr_read.cc
@@ -7,9 +7,22 @@ uint32_t      SPI_CR1_ARR[SPI_INTF_TOTAL];
 uint32_t      SPI_CRCPR_ARR[SPI_INTF_TOTAL];
 struct spi_dr SPI_DR_ARR[SPI_INTF_TOTAL];
 
+///
+/// \brief The gtest_bmi270_gyr_read test fixture class.
+///
 class gtest_bmi270_gyr_read : public ::testing::Test
 {
     protected:
+        static void SetUpTestSuite()
+        {
+            dev = bmi270_dev_get();
+        }
+
+        static void TearDownTestSuite()
+        {
+            dev = nullptr;
+        }
+
         void SetUp() override
         {
             if (::testing::UnitTest::GetInstance()->current_test_info()->name() ==
@@ -28,7 +41,26 @@ class gtest_bmi270_gyr_read : public ::testing::Test
                 SPI_DR_ARR[SPI1].buf[6] = 0x66;
             }
         }
+
+        void TearDown() override
+        {
+            if (::testing::UnitTest::GetInstance()->current_test_info()->name() ==
+                std::string("procedure"))
+            {
+                SPI_DR_ARR[SPI1].buf[0] = 0x00;
+                SPI_DR_ARR[SPI1].buf[1] = 0x00;
+                SPI_DR_ARR[SPI1].buf[2] = 0x00;
+                SPI_DR_ARR[SPI1].buf[3] = 0x00;
+                SPI_DR_ARR[SPI1].buf[4] = 0x00;
+                SPI_DR_ARR[SPI1].buf[5] = 0x00;
+                SPI_DR_ARR[SPI1].buf[6] = 0x00;
+            }
+        }
+
+        static bmi270_dev *dev;
 };
+
+struct bmi270_dev *gtest_bmi270_gyr_read::dev = nullptr;
 
 ///
 /// \brief This test performs the bmi270 gyroscope read procedure.
@@ -40,24 +72,21 @@ TEST_F(gtest_bmi270_gyr_read, procedure)
     int16_t y;
     int16_t z;
 
-    struct bmi270_dev *dev = NULL;
-    dev = bmi270_get_dev();
-
-    res = bmi270_spi_ctrl_asg(dev);
+    res = bmi270_spi_ctrl_asg(gtest_bmi270_gyr_read::dev);
     EXPECT_EQ(res, BMI270_RES_OK);
 
-    res = bmi270_gyr_read(dev);
+    res = bmi270_gyr_read(gtest_bmi270_gyr_read::dev);
     EXPECT_EQ(res, BMI270_RES_OK);
 
-    res = bmi270_gyr_get_x(dev, &x);
+    res = bmi270_gyr_get_x(gtest_bmi270_gyr_read::dev, &x);
     EXPECT_EQ(res, BMI270_RES_OK);
     EXPECT_EQ(x, (int16_t)(0x2211));
 
-    res = bmi270_gyr_get_y(dev, &y);
+    res = bmi270_gyr_get_y(gtest_bmi270_gyr_read::dev, &y);
     EXPECT_EQ(res, BMI270_RES_OK);
     EXPECT_EQ(y, (int16_t)(0x4433));
 
-    res = bmi270_gyr_get_z(dev, &z);
+    res = bmi270_gyr_get_z(gtest_bmi270_gyr_read::dev, &z);
     EXPECT_EQ(res, BMI270_RES_OK);
     EXPECT_EQ(z, (int16_t)(0x6655));
 }
