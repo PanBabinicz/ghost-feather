64d633609ea58013bdc3bb2a764dcd05a2dc0bfa
km1t4h <km1t4h@gmail.com>
2025-08-30 20:22:56 +0200

    feat: Improve resources sharing inside bmi270_gyr_set_x test fixture class

--- tests/gtest/controller/bmi270/gyr_set_x/gyr_set_x.cc
+++ tests/gtest/controller/bmi270/gyr_set_x/gyr_set_x.cc
@@ -8,20 +8,46 @@ uint32_t      SPI_CRCPR_ARR[SPI_INTF_TOTAL];
 struct spi_dr SPI_DR_ARR[SPI_INTF_TOTAL];
 
 ///
+/// \brief The gtest_bmi270_gyr_set_x test fixture class.
+///
+class gtest_bmi270_gyr_set_x : public ::testing::Test
+{
+    protected:
+        static void SetUpTestSuite()
+        {
+            dev = bmi270_dev_get();
+        }
+
+        static void TearDownTestSuite()
+        {
+            dev = nullptr;
+        }
+
+        void SetUp() override
+        {
+        }
+
+        void TearDown() override
+        {
+        }
+
+        static bmi270_dev *dev;
+};
+
+struct bmi270_dev *gtest_bmi270_gyr_set_x::dev = nullptr;
+
+///
 /// \brief This test performs the bmi270 gyroscope set x axis procedure.
 ///
-TEST(gtest_bmi270_gyr_set_x, procedure)
+TEST_F(gtest_bmi270_gyr_set_x, procedure)
 {
     bmi270_res_t res;
     int16_t x;
 
-    struct bmi270_dev *dev = NULL;
-    dev = bmi270_get_dev();
-
-    res = bmi270_gyr_set_x(dev, 0xd00d);
+    res = bmi270_gyr_set_x(gtest_bmi270_gyr_set_x::dev, 0xd00d);
     EXPECT_EQ(res, BMI270_RES_OK);
 
-    res = bmi270_gyr_get_x(dev, &x);
+    res = bmi270_gyr_get_x(gtest_bmi270_gyr_set_x::dev, &x);
     EXPECT_EQ(res, BMI270_RES_OK);
 
     EXPECT_EQ(x, (int16_t)0xd00d);
@@ -31,12 +57,9 @@ TEST(gtest_bmi270_gyr_set_x, procedure)
 /// \brief This test checks the null pointer protection inside bmi270 gyroscope
 ///        set x function.
 ///
-TEST(gtest_bmi270_gyr_set_x, null_pointer_protection)
+TEST_F(gtest_bmi270_gyr_set_x, null_pointer_protection)
 {
     bmi270_res_t res;
-
-    struct bmi270_dev *dev = NULL;
-    dev = bmi270_get_dev();
 
     res = bmi270_gyr_set_x(NULL, 0xd00d);
     EXPECT_EQ(res, BMI270_RES_ERR);
