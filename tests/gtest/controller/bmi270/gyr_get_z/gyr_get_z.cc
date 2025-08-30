deef3e17e4964938e2a3493222ea0aef6240e025
km1t4h <km1t4h@gmail.com>
2025-08-30 20:22:02 +0200

    feat: Improve resources sharing inside bmi270_gyr_get_z test fixture class

--- tests/gtest/controller/bmi270/gyr_get_z/gyr_get_z.cc
+++ tests/gtest/controller/bmi270/gyr_get_z/gyr_get_z.cc
@@ -7,21 +7,46 @@ uint32_t      SPI_CR1_ARR[SPI_INTF_TOTAL];
 uint32_t      SPI_CRCPR_ARR[SPI_INTF_TOTAL];
 struct spi_dr SPI_DR_ARR[SPI_INTF_TOTAL];
 
+///
+/// \brief The gtest_bmi270_gyr_get_z test fixture class.
+///
 class gtest_bmi270_gyr_get_z : public ::testing::Test
 {
     protected:
+        static void SetUpTestSuite()
+        {
+            dev = bmi270_dev_get();
+        }
+
+        static void TearDownTestSuite()
+        {
+            dev = nullptr;
+        }
+
         void SetUp() override
         {
             if (::testing::UnitTest::GetInstance()->current_test_info()->name() ==
                 std::string("procedure"))
             {
-                struct bmi270_dev *dev = bmi270_get_dev();
                 int16_t z = 0xeeee;
-
                 (void)bmi270_gyr_set_z(dev, z);
             }
         }
+
+        void TearDown() override
+        {
+            if (::testing::UnitTest::GetInstance()->current_test_info()->name() ==
+                std::string("procedure"))
+            {
+                int16_t z = 0x0000;
+                (void)bmi270_gyr_set_z(dev, z);
+            }
+        }
+
+        static bmi270_dev *dev;
 };
+
+struct bmi270_dev *gtest_bmi270_gyr_get_z::dev = nullptr;
 
 ///
 /// \brief This test performs the bmi270 gyroscope get z axis procedure.
@@ -31,10 +56,7 @@ TEST_F(gtest_bmi270_gyr_get_z, procedure)
     bmi270_res_t res;
     int16_t z;
 
-    struct bmi270_dev *dev = NULL;
-    dev = bmi270_get_dev();
-
-    res = bmi270_gyr_get_z(dev, &z);
+    res = bmi270_gyr_get_z(gtest_bmi270_gyr_get_z::dev, &z);
     EXPECT_EQ(res, BMI270_RES_OK);
 
     EXPECT_EQ(z, (int16_t)0xeeee);
@@ -49,12 +71,9 @@ TEST_F(gtest_bmi270_gyr_get_z, null_pointer_protection)
     bmi270_res_t res;
     int16_t z;
 
-    struct bmi270_dev *dev = NULL;
-    dev = bmi270_get_dev();
-
     res = bmi270_gyr_get_z(NULL, &z);
     EXPECT_EQ(res, BMI270_RES_ERR);
 
-    res = bmi270_gyr_get_z(dev, NULL);
+    res = bmi270_gyr_get_z(gtest_bmi270_gyr_get_z::dev, NULL);
     EXPECT_EQ(res, BMI270_RES_ERR);
 }
