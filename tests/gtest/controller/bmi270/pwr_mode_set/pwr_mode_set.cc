a8ddc392e720ee0a9e6ccbbd59f6db00e5e92815
km1t4h <km1t4h@gmail.com>
2025-08-31 22:34:42 +0200

    feat: Improve resources sharing inside bmi270_pwr_mode_set test fixture class

--- tests/gtest/controller/bmi270/pwr_mode_set/pwr_mode_set.cc
+++ tests/gtest/controller/bmi270/pwr_mode_set/pwr_mode_set.cc
@@ -7,17 +7,34 @@ uint32_t      SPI_CR1_ARR[SPI_INTF_TOTAL];
 uint32_t      SPI_CRCPR_ARR[SPI_INTF_TOTAL];
 struct spi_dr SPI_DR_ARR[SPI_INTF_TOTAL];
 
+///
+/// \brief The gtest_bmi270_pwr_mode_set test fixture class.
+///
 class gtest_bmi270_pwr_mode_set : public ::testing::Test
 {
     protected:
+        static void SetUpTestSuite()
+        {
+            dev = bmi270_dev_get();
+        }
+
+        static void TearDownTestSuite()
+        {
+            dev = nullptr;
+        }
+
         void SetUp() override
         {
-            if (::testing::UnitTest::GetInstance()->current_test_info()->name() ==
-                std::string("procedure"))
-            {
-            }
         }
+
+        void TearDown() override
+        {
+        }
+
+        static bmi270_dev *dev;
 };
+
+struct bmi270_dev *gtest_bmi270_pwr_mode_set::dev = nullptr;
 
 ///
 /// \brief This test performs the bmi270 power mode set procedure.
@@ -33,24 +50,22 @@ TEST_F(gtest_bmi270_pwr_mode_set, procedure)
 TEST_F(gtest_bmi270_pwr_mode_set, null_pointer_protection)
 {
     bmi270_res_t res;
-    struct bmi270_dev *dev = NULL;
 
     const struct bmi270_pwr_mode_conf *pwr_mode_conf =
       bmi270_pwr_mode_get_conf(BMI270_PWR_MODE_SUSP);
-    dev = bmi270_get_dev();
 
     /* The bmi270 device is NULL. */
     res = bmi270_pwr_mode_set(NULL, pwr_mode_conf);
     EXPECT_EQ(res, BMI270_RES_ERR);
 
     /* The spi control device is NULL. */
-    res = bmi270_pwr_mode_set(dev, pwr_mode_conf);
+    res = bmi270_pwr_mode_set(gtest_bmi270_pwr_mode_set::dev, pwr_mode_conf);
     EXPECT_EQ(res, BMI270_RES_ERR);
 
-    res = bmi270_spi_ctrl_asg(dev);
+    res = bmi270_spi_ctrl_asg(gtest_bmi270_pwr_mode_set::dev);
     EXPECT_EQ(res, BMI270_RES_OK);
 
     /* The pwr_mode_conf is NULL. */
-    res = bmi270_pwr_mode_set(dev, NULL);
+    res = bmi270_pwr_mode_set(gtest_bmi270_pwr_mode_set::dev, NULL);
     EXPECT_EQ(res, BMI270_RES_ERR);
 }
