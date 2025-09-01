5dbe883734aacca1b7065c5017fb95d859817785
km1t4h <km1t4h@gmail.com>
2025-09-01 21:21:19 +0200

    feat: Improve resources sharing inside bmi270_stat_set test fixture class

--- tests/gtest/controller/bmi270/stat_set/stat_set.cc
+++ tests/gtest/controller/bmi270/stat_set/stat_set.cc
@@ -8,20 +8,46 @@ uint32_t      SPI_CRCPR_ARR[SPI_INTF_TOTAL];
 struct spi_dr SPI_DR_ARR[SPI_INTF_TOTAL];
 
 ///
+/// \brief The gtest_bmi270_stat_set test fixture class.
+///
+class gtest_bmi270_stat_set : public ::testing::Test
+{
+    protected:
+        static void SetUpTestSuite()
+        {
+            dev = bmi270_dev_get();
+        }
+
+        static void TearDownTestSuite()
+        {
+            dev = nullptr;
+        }
+
+        void SetUp() override
+        {
+        }
+
+        void TearDown() override
+        {
+        }
+
+        static bmi270_dev *dev;
+};
+
+struct bmi270_dev *gtest_bmi270_stat_set::dev = nullptr;
+
+///
 /// \brief This test performs the bmi270 status set procedure.
 ///
-TEST(gtest_bmi270_stat_set, procedure)
+TEST_F(gtest_bmi270_stat_set, procedure)
 {
     bmi270_res_t res;
     bmi270_stat_t stat;
 
-    struct bmi270_dev *dev = NULL;
-    dev = bmi270_get_dev();
-
-    res = bmi270_stat_set(dev, BMI270_STAT_INIT);
+    res = bmi270_stat_set(gtest_bmi270_stat_set::dev, BMI270_STAT_INIT);
     EXPECT_EQ(res, BMI270_RES_OK);
 
-    res = bmi270_stat_get(dev, &stat);
+    res = bmi270_stat_get(gtest_bmi270_stat_set::dev, &stat);
     EXPECT_EQ(res, BMI270_RES_OK);
 
     EXPECT_EQ(stat, BMI270_STAT_INIT);
@@ -31,17 +57,14 @@ TEST(gtest_bmi270_stat_set, procedure)
 /// \brief This test checks the range of status argument inside bmi270
 ///        status set procedure.
 ///
-TEST(gtest_bmi270_stat_set, range)
+TEST_F(gtest_bmi270_stat_set, range)
 {
     bmi270_res_t res;
 
-    struct bmi270_dev *dev = NULL;
-    dev = bmi270_get_dev();
-
-    res = bmi270_stat_set(dev, (bmi270_stat_t)(BMI270_STAT_BEGIN - 1));
+    res = bmi270_stat_set(gtest_bmi270_stat_set::dev, (bmi270_stat_t)(BMI270_STAT_BEGIN - 1));
     EXPECT_EQ(res, BMI270_RES_ERR);
 
-    res = bmi270_stat_set(dev, BMI270_STAT_TOTAL);
+    res = bmi270_stat_set(gtest_bmi270_stat_set::dev, BMI270_STAT_TOTAL);
     EXPECT_EQ(res, BMI270_RES_ERR);
 }
 
@@ -49,12 +72,9 @@ TEST(gtest_bmi270_stat_set, range)
 /// \brief This test checks the null pointer protection inside bmi270 status
 ///        set function.
 ///
-TEST(gtest_bmi270_stat_set, null_pointer_protection)
+TEST_F(gtest_bmi270_stat_set, null_pointer_protection)
 {
     bmi270_res_t res;
-
-    struct bmi270_dev *dev = NULL;
-    dev = bmi270_get_dev();
 
     res = bmi270_stat_set(NULL, BMI270_STAT_INIT);
     EXPECT_EQ(res, BMI270_RES_ERR);
