492674c2ae156bc7344265594b451a4bce699780
km1t4h <km1t4h@gmail.com>
2025-09-01 21:27:09 +0200

    feat: Improve resources sharing inside bmi270_time_set test fixture class

--- tests/gtest/controller/bmi270/time_set/time_set.cc
+++ tests/gtest/controller/bmi270/time_set/time_set.cc
@@ -8,20 +8,46 @@ uint32_t      SPI_CRCPR_ARR[SPI_INTF_TOTAL];
 struct spi_dr SPI_DR_ARR[SPI_INTF_TOTAL];
 
 ///
+/// \brief The gtest_bmi270_time_set test fixture class.
+///
+class gtest_bmi270_time_set : public ::testing::Test
+{
+    protected:
+        static void SetUpTestSuite()
+        {
+            dev = bmi270_dev_get();
+        }
+
+        static void TearDownTestSuite()
+        {
+            dev = nullptr;
+        }
+
+        void SetUp() override
+        {
+        }
+
+        void TearDown() override
+        {
+        }
+
+        static bmi270_dev *dev;
+};
+
+struct bmi270_dev *gtest_bmi270_time_set::dev = nullptr;
+
+///
 /// \brief This test performs the bmi270 time set procedure.
 ///
-TEST(gtest_bmi270_time_set, procedure)
+TEST_F(gtest_bmi270_time_set, procedure)
 {
     bmi270_res_t res;
     uint32_t time;
 
-    struct bmi270_dev *dev = NULL;
-    dev = bmi270_get_dev();
-
-    res = bmi270_time_set(dev, 0x00beef00);
+    res = bmi270_time_set(gtest_bmi270_time_set::dev, 0x00beef00);
     EXPECT_EQ(res, BMI270_RES_OK);
 
-    res = bmi270_time_get(dev, &time);
+    res = bmi270_time_get(gtest_bmi270_time_set::dev, &time);
     EXPECT_EQ(res, BMI270_RES_OK);
 
     EXPECT_EQ(time, 0x00beef00);
@@ -30,13 +56,10 @@ TEST(gtest_bmi270_time_set, procedure)
 ///
 /// \brief This test checks the null pointer protection inside bmi270 time set function.
 ///
-TEST(gtest_bmi270_time_set, null_pointer_protection)
+TEST_F(gtest_bmi270_time_set, null_pointer_protection)
 {
     bmi270_res_t res;
     uint32_t time;
-
-    struct bmi270_dev *dev = NULL;
-    dev = bmi270_get_dev();
 
     res = bmi270_time_set(NULL, 0x00b00b00);
     EXPECT_EQ(res, BMI270_RES_ERR);
