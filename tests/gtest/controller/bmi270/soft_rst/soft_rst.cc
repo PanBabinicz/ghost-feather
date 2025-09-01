9d34bbf22a1a19b1c160b0756c9553ca21a46c56
km1t4h <km1t4h@gmail.com>
2025-09-01 21:18:32 +0200

    feat: Improve resources sharing inside bmi270_soft_rst test fixture class

--- tests/gtest/controller/bmi270/soft_rst/soft_rst.cc
+++ tests/gtest/controller/bmi270/soft_rst/soft_rst.cc
@@ -8,32 +8,64 @@ uint32_t      SPI_CRCPR_ARR[SPI_INTF_TOTAL];
 struct spi_dr SPI_DR_ARR[SPI_INTF_TOTAL];
 
 ///
+/// \brief The gtest_bmi270_soft_rst test fixture class.
+///
+class gtest_bmi270_soft_rst : public ::testing::Test
+{
+    protected:
+        static void SetUpTestSuite()
+        {
+            dev = bmi270_dev_get();
+        }
+
+        static void TearDownTestSuite()
+        {
+            dev = nullptr;
+        }
+
+        void SetUp() override
+        {
+            if (::testing::UnitTest::GetInstance()->current_test_info()->name() ==
+                std::string("procedure"))
+            {
+                (void)bmi270_stat_set(dev, BMI270_STAT_INIT);
+            }
+        }
+
+        void TearDown() override
+        {
+            if (::testing::UnitTest::GetInstance()->current_test_info()->name() ==
+                std::string("procedure"))
+            {
+                (void)bmi270_stat_set(dev, BMI270_STAT_DEINIT);
+            }
+        }
+
+        static bmi270_dev *dev;
+};
+
+struct bmi270_dev *gtest_bmi270_soft_rst::dev = nullptr;
+
+///
 /// \brief This test performs the bmi270 soft reset procedure.
 ///
-TEST(gtest_bmi270_soft_rst, procedure)
+TEST_F(gtest_bmi270_soft_rst, procedure)
 {
     bmi270_res_t res;
     bmi270_stat_t stat;
 
-    struct bmi270_dev *dev = NULL;
-    dev = bmi270_get_dev();
-
-    res = bmi270_spi_ctrl_asg(dev);
+    res = bmi270_spi_ctrl_asg(gtest_bmi270_soft_rst::dev);
     EXPECT_EQ(res, BMI270_RES_OK);
 
-    res = bmi270_stat_set(dev, BMI270_STAT_INIT);
-    EXPECT_EQ(res, BMI270_RES_OK);
-
-    res = bmi270_stat_get(dev, &stat);
+    res = bmi270_stat_get(gtest_bmi270_soft_rst::dev, &stat);
     EXPECT_EQ(res, BMI270_RES_OK);
     EXPECT_EQ(stat, BMI270_STAT_INIT);
 
-    res = bmi270_soft_rst(dev);
+    res = bmi270_soft_rst(gtest_bmi270_soft_rst::dev);
     EXPECT_EQ(res, BMI270_RES_OK);
 
-    res = bmi270_stat_get(dev, &stat);
+    res = bmi270_stat_get(gtest_bmi270_soft_rst::dev, &stat);
     EXPECT_EQ(res, BMI270_RES_OK);
-
     EXPECT_EQ(stat, BMI270_STAT_DEINIT);
 }
 
@@ -41,7 +73,7 @@ TEST(gtest_bmi270_soft_rst, procedure)
 /// \brief This test checks the null pointer protection inside bmi270 soft
 ///        reset function.
 ///
-TEST(gtest_bmi270_soft_rst, null_pointer_protection)
+TEST_F(gtest_bmi270_soft_rst, null_pointer_protection)
 {
     bmi270_res_t res;
 
