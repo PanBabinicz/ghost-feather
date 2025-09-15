ef0eacba31f50943498855aec45e1abaf30e028e
km1t4h <km1t4h@gmail.com>
2025-09-15 23:56:11 +0200

    feat: Add bmi270 pwr_mode_set procedure unit test #1

--- tests/gtest/controller/bmi270/pwr_mode_set/pwr_mode_set.cc
+++ tests/gtest/controller/bmi270/pwr_mode_set/pwr_mode_set.cc
@@ -38,9 +38,38 @@ class gtest_bmi270_pwr_mode_set : public ::testing::Test
                  * to fifo during transmission. The fifo has two indexes, rx and tx.
                  * It always starts reading from rx_idx which is 0. */
 
-                /* buf[0] and buf[1] will be discarded. */
+                /* Read the PWR_CONF and PWR_CTRL registers value
+                 * The buf[0] is dummy byte and will be discarded.
+                 * The buf[1] corresponds to PWR_CONF, reset value is 0x03.
+                 * The buf[2] corresponds to PWR_CTRL, reset value is 0x00. */
                 SPI_DR_ARR[SPI1].buf[0] = 0x00;
-                SPI_DR_ARR[SPI1].buf[1] = 0x00;
+                SPI_DR_ARR[SPI1].buf[1] = 0x03;
+                SPI_DR_ARR[SPI1].buf[2] = 0x00;
+
+                /* Read the ACC_CONF register value.
+                 * The buf[3] is dummy byte and will be discarded.
+                 * The buf[4] corresponds to ACC_CONF, reset value is 0xa8. */
+                SPI_DR_ARR[SPI1].buf[3] = 0x00;
+                SPI_DR_ARR[SPI1].buf[4] = 0xa8;
+
+                /* Read the GYR_CONF register value.
+                 * The buf[5] is dummy byte and will be discarded.
+                 * The buf[6] corresponds to GYR_CONF, reset value is 0xa9. */
+                SPI_DR_ARR[SPI1].buf[5] = 0x00;
+                SPI_DR_ARR[SPI1].buf[6] = 0xa9;
+
+                /* Set tx_idx to point to first the unoccupied place in the buffer. */
+                SPI_DR_ARR[SPI1].tx_idx = 7;
+
+                /* Buffers after write operations inside bmi270_pwr_mode_set,
+                 * And for this scenario:
+                 * buf[7]  = BMI270_REG_PWR_CONF
+                 * buf[8]  = 0x03
+                 * buf[9]  = 0x00
+                 * buf[10] = BMI270_REG_ACC_CONF
+                 * buf[11] = 0xa8
+                 * buf[12] = BMI270_REG_GYR_CONF
+                 * buf[13] = 0xa9 */
         }
 
         void TearDown() override
@@ -59,6 +88,24 @@ struct spi_ctrl_dev *gtest_bmi270_pwr_mode_set::spi_ctrl = nullptr;
 ///
 TEST_F(gtest_bmi270_pwr_mode_set, procedure)
 {
+    bmi270_res_t res;
+
+    const struct bmi270_pwr_mode_conf *pwr_mode_conf =
+      bmi270_pwr_mode_get_conf(BMI270_PWR_MODE_SUSP);
+
+    res = bmi270_spi_ctrl_asg(gtest_bmi270_pwr_mode_set::bmi270);
+    EXPECT_EQ(res, BMI270_RES_OK);
+
+    res = bmi270_pwr_mode_set(gtest_bmi270_pwr_mode_set::bmi270, pwr_mode_conf);
+    EXPECT_EQ(res, BMI270_RES_OK);
+
+    EXPECT_EQ(SPI_DR_ARR[SPI1].buf[7],  BMI270_REG_PWR_CONF);
+    EXPECT_EQ(SPI_DR_ARR[SPI1].buf[8],  0x03);
+    EXPECT_EQ(SPI_DR_ARR[SPI1].buf[9],  0x00);
+    EXPECT_EQ(SPI_DR_ARR[SPI1].buf[10], BMI270_REG_ACC_CONF);
+    EXPECT_EQ(SPI_DR_ARR[SPI1].buf[11], 0xa8);
+    EXPECT_EQ(SPI_DR_ARR[SPI1].buf[12], BMI270_REG_GYR_CONF);
+    EXPECT_EQ(SPI_DR_ARR[SPI1].buf[13], 0xa9);
 }
 
 ///
