2dcaf8ce27aeda6a19e43859d6782e923ccaeb23
km1t4h <km1t4h@gmail.com>
2025-09-30 23:48:25 +0200

    feat: Add systick init, deinit and delay_us functions

--- shared/ghost_feather_common.h
+++ shared/ghost_feather_common.h
@@ -4,29 +4,66 @@
 #include <stdint.h>
 #include "libopencm3/cm3/systick.h"
 
-#define GHOST_FEATHER_COMMON_START_APP              (0U)
+#define GHOST_FEATHER_COMMON_START_APP              (1U)
 #define GHOST_FEATHER_COMMON_PROCESSOR_FREQUENCY    (216000000U)
-#define GHOST_FEATHER_COMMON_CYCLE_TIME             (4.62962962962963F)
+#define GHOST_FEATHER_COMMON_SINGLE_CYCLE           (4.62962962962963F)
+#define GHOST_FEATHER_COMMON_US_IN_CYCLES           (216U)
+#define GHOST_FEATHER_COMMON_MS_IN_CYCLES           (216000U)
+
+///*************************************************************************************************
+/// Private objects - definition.
+///*************************************************************************************************
+///
+/// \brief The systick counter variable. It updates in _systick_handler.
+///
+static uint32_t systick_cntr;
+
+///*************************************************************************************************
+/// Private functions - declaration.
+///*************************************************************************************************
+///
+/// \brief Initializes the systick counter.
+///
+/// \param[in] rld The systick counter reload value.
+///
+static inline void systick_init(uint32_t rld);
 
 ///
-/// \brief Delays execution for a specified number of milliseconds.
+/// \brief Deinitializes the systick counter.
 ///
-/// \param[in] ms The number of milliseconds.
+/// \param[in] rld The systick counter reload value.
 ///
-static inline void systick_delay_ms(uint32_t ms);
+static inline void systick_deinit(void);
 
-static inline void systick_delay_ms(uint32_t ms)
+///
+/// \brief Delays execution for a specified number of microseconds.
+///
+/// \param[in] us The number of microseconds.
+///
+static inline void systick_delay_us(uint32_t ms);
+
+///*************************************************************************************************
+/// Private functions - definition.
+///*************************************************************************************************
+static inline void systick_init(uint32_t rld)
 {
-    uint32_t rld_val;
-    uint32_t ns = ms * 1000000;
-
-    rld_val = (uint32_t)((float)ns / GHOST_FEATHER_COMMON_CYCLE_TIME);
-
-    systick_set_reload(rld_val);
+    systick_counter_disable();
+    systick_interrupt_enable();
+    systick_set_reload(rld - 1);
     systick_clear();
+    systick_counter_enable();
+}
 
-    /* Wait for the count flag to be set */
-    while (!systick_get_countflag());
+static inline void systick_deinit(void)
+{
+    systick_counter_disable();
+    systick_interrupt_disable();
+}
+
+static inline void systick_delay_us(uint32_t us)
+{
+    systick_cntr = 0;
+    while (systick_cntr < us);
 }
 
 #endif  /* _GHOST_FEATHER_COMMON_H */
