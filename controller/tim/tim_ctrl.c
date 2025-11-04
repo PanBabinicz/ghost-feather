bb82ec80e7fba2d14dbae7c515684725df1e77b0
km1t4h <km1t4h@gmail.com>
2025-11-04 22:08:04 +0100

    wip: Add structure of each timer

--- controller/tim/tim_ctrl.c
+++ controller/tim/tim_ctrl.c
@@ -6,7 +6,70 @@
 ///
 /// \brief
 ///
-struct tim_ctrl_dev
+struct tim_ctrl_ch32
 {
-}
+    uint32_t ar;
+    uint32_t cnt;
+    uint32_t ccr;
+    uint16_t psc;
+};
+
+///
+/// \brief
+///
+struct tim_ctrl_ch16
+{
+    uint16_t ar;
+    uint16_t cnt;
+    uint16_t ccr;
+    uint16_t psc;
+};
+
+///
+/// \brief
+///
+struct tim_ctrl_adv4
+{
+    tim_ctrl_ch16 ch_arr[4];
+};
+
+///
+/// \brief
+///
+struct tim_ctrl_gp1
+{
+    tim_ctrl_ch16 ch_arr[1];
+};
+
+///
+/// \brief
+///
+struct tim_ctrl_gp2
+{
+    tim_ctrl_ch16 ch_arr[2];
+};
+
+///
+/// \brief
+///
+struct tim_ctrl_gp4_32
+{
+    tim_ctrl_ch32 ch_arr[4];
+};
+
+///
+/// \brief
+///
+struct tim_ctrl_gp4_16
+{
+    tim_ctrl_ch16 ch_arr[4];
+};
+
+///
+/// \brief
+///
+struct tim_ctrl_base1
+{
+    tim_ctrl_ch16 ch_arr[1];
+};
 
