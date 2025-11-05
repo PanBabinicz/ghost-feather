3ff506625e156fb36b8d3e791504099c0c037c96
km1t4h <km1t4h@gmail.com>
2025-11-05 23:33:14 +0100

    wip: Add is_init flags and information about mapped timers inside the system

--- controller/tim/tim_ctrl.c
+++ controller/tim/tim_ctrl.c
@@ -31,6 +31,7 @@ struct tim_ctrl_ch16
 struct tim_ctrl_adv4
 {
     tim_ctrl_ch16 ch_arr[4];
+    bool is_init;
 };
 
 ///
@@ -39,6 +40,7 @@ struct tim_ctrl_adv4
 struct tim_ctrl_gp1
 {
     tim_ctrl_ch16 ch_arr[1];
+    bool is_init;
 };
 
 ///
@@ -47,6 +49,7 @@ struct tim_ctrl_gp1
 struct tim_ctrl_gp2
 {
     tim_ctrl_ch16 ch_arr[2];
+    bool is_init;
 };
 
 ///
@@ -55,6 +58,7 @@ struct tim_ctrl_gp2
 struct tim_ctrl_gp4_32
 {
     tim_ctrl_ch32 ch_arr[4];
+    bool is_init;
 };
 
 ///
@@ -63,6 +67,7 @@ struct tim_ctrl_gp4_32
 struct tim_ctrl_gp4_16
 {
     tim_ctrl_ch16 ch_arr[4];
+    bool is_init;
 };
 
 ///
@@ -71,5 +76,21 @@ struct tim_ctrl_gp4_16
 struct tim_ctrl_base1
 {
     tim_ctrl_ch16 ch_arr[1];
+    bool is_init;
 };
 
+///
+/// Used timers inside ghost feather system:
+///
+/// MOTOR0 -> TIM4_CH4  (PB9)   gp4_16
+/// MOTOR1 -> TIM4_CH3  (PB8)   gp4_16
+/// MOTOR2 -> TIM4_CH2  (PB7)   gp4_16
+/// MOTOR3 -> TIM4_CH1  (PB6)   gp4_16
+///
+/// RFCH0  -> TIM8_CH4  (PC9)   adv4
+/// RFCH1  -> TIM8_CH3  (PC8)   adv4
+/// RFCH2  -> TIM8_CH2  (PC7)   adv4
+/// RFCH3  -> TIM8_CH1  (PC6)   adv4
+/// RFCH4  -> TIM12_CH2 (PB15)  gp2
+/// RFCH5  -> TIM12_CH1 (PB14)  gp2
+///
