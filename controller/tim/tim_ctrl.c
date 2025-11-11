08a9c39c75498cdf3692171c0916e58430fdd51a
km1t4h <km1t4h@gmail.com>
2025-11-11 21:34:48 +0100

    wip: Add base timers register definitions #1

--- controller/tim/tim_ctrl.c
+++ controller/tim/tim_ctrl.c
@@ -1273,6 +1273,119 @@ union tim_ctrl_gpx_tim11_or
 ///
 /// \brief
 ///
+union tim_ctrl_base1_timx_cr1
+{
+    uint16_t r;
+    struct bit_field
+    {
+        uint16_t cen      : 1;
+        uint16_t udis     : 1;
+        uint16_t urs      : 1;
+        uint16_t opm      : 1;
+        uint16_t          : 3;
+        uint16_t arpe     : 1;
+        uint16_t          : 3;
+        uint16_t uifremap : 1;
+        uint16_t          : 4;
+    };
+};
+
+///
+/// \brief
+///
+union tim_ctrl_base1_timx_cr2
+{
+    uint16_t r;
+    struct bit_field
+    {
+        uint16_t          : 4;
+        uint16_t mms      : 3;
+        uint16_t          : 9;
+    };
+};
+
+///
+/// \brief
+///
+union tim_ctrl_base1_timx_dier
+{
+    uint16_t r;
+    struct bit_field
+    {
+        uint16_t uie      : 1;
+        uint16_t          : 7;
+        uint16_t ude      : 1;
+        uint16_t          : 7;
+    };
+};
+
+///
+/// \brief
+///
+union tim_ctrl_base1_timx_sr
+{
+    uint16_t r;
+    struct bit_field
+    {
+        uint16_t uif      : 1;
+        uint16_t          : 15;
+    };
+};
+
+///
+/// \brief
+///
+union tim_ctrl_base1_timx_egr
+{
+    uint16_t r;
+    struct bit_field
+    {
+        uint16_t ug       : 1;
+        uint16_t          : 15;
+    };
+};
+
+///
+/// \brief
+///
+union tim_ctrl_base1_timx_cnt
+{
+    uint32_t r;
+    struct bit_field
+    {
+        uint32_t cnt      : 16;
+        uint32_t          : 15;
+        uint32_t uifcpy   : 1;
+    };
+};
+
+///
+/// \brief
+///
+union tim_ctrl_base1_timx_psc
+{
+    uint16_t r;
+    struct bit_field
+    {
+        uint16_t psc      : 16;
+    };
+};
+
+///
+/// \brief
+///
+union tim_ctrl_base1_timx_arr
+{
+    uint16_t r;
+    struct bit_field
+    {
+        uint16_t arr      : 16;
+    };
+};
+
+///
+/// \brief
+///
 struct tim_ctrl_ch32
 {
     uint32_t ar;
