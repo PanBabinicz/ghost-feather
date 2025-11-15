91d7aa0dbda46a680cc077a58f96132d35dae060
km1t4h <km1t4h@gmail.com>
2025-11-15 23:49:38 +0100

    feat: Add tim ctrl common header file

---
+++ controller/tim/tim_ctrl_common.h
@@ -0,0 +1,23 @@
+#ifndef _TIM_CTRL_COMMON_H
+#define _TIM_CTRL_COMMON_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif  /* __cplusplus */
+
+///
+/// \breif The TIM controller result type.
+///
+typedef enum tim_ctrl_res
+{
+    TIM_CTRL_RES_OK = 0,
+    TIM_CTRL_RES_ERR,
+} tim_ctrl_res_t;
+
+#ifdef __cplusplus
+}
+#endif  /* __cplusplus */
+
+#endif  /* _TIM_CTRL_COMMON_H */
