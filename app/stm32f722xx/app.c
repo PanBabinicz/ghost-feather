04aa7d671b675f514ec9de1f254561548a16ec6f
km1t4h <km1t4h@gmail.com>
2025-12-17 23:54:43 +0100

    feat: Add TIM devices inside application

--- app/stm32f722xx/app.c
+++ app/stm32f722xx/app.c
@@ -51,6 +51,16 @@ void app_start(void)
     tim_ctrl_init();
 
     struct bmi270_dev *bmi270 = bmi270_dev_get();
+    struct tim_ctrl_dev *tim_ctrl_dev_arr = tim_ctrl_dev_arr_get();
+
+    volatile struct tim_ctrl_gpx_tim2345_dev *tim4 =
+        (volatile struct tim_ctrl_gpx_tim2345_dev *)tim_ctrl_dev_arr[TIM_CTRL_INST_TIM4].tim;
+
+    volatile struct tim_ctrl_adv6_tim18_dev *tim8 =
+        (volatile struct tim_ctrl_adv6_tim18_dev *)tim_ctrl_dev_arr[TIM_CTRL_INST_TIM8].tim;
+
+    volatile struct tim_ctrl_gpx_tim912_dev *tim12 =
+        (volatile struct tim_ctrl_gpx_tim912_dev *)tim_ctrl_dev_arr[TIM_CTRL_INST_TIM12].tim;
 
     /* Never return */
     while (1);
