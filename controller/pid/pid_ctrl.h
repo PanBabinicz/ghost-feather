58c76cfb8105aca723ee69a3751951451376ae02
km1t4h <km1t4h@gmail.com>
2025-12-19 19:33:42 +0100

    feat: Add pid_ctrl files template

---
+++ controller/pid/pid_ctrl.h
@@ -0,0 +1,15 @@
+#ifndef _PID_CTRL_H
+#define _PID_CTRL_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif  /* __cplusplus */
+
+#ifdef __cplusplus
+}
+#endif  /* __cplusplus */
+
+#endif  /* _PID_CTRL_H */
+
