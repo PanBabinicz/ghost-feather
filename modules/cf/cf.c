3bd024b17e916fc6b0f5c1e03d9664d8a1029ea0
km1t4h <km1t4h@gmail.com>
2026-03-21 23:41:24 +0100

    feat: Change passing instance as an argument to passing a pointer

--- modules/cf/cf.c
+++ modules/cf/cf.c
@@ -1,72 +1,48 @@
 #include "cf.h"
 
 ///***********************************************************************************************************
-/// Private objects - declaration.
-///***********************************************************************************************************
-///
-/// \brief The complementary filter struct.
-///
-struct cf
-{
-    float32_t alpha;
-    float32_t ang;
-};
-
-///***********************************************************************************************************
-/// Private objects - definition.
-///***********************************************************************************************************
-///
-/// \brief The complementary filters array.
-///
-static struct cf cf_arr[CF_INST_TOTAL] =
-{
-    [CF_INST_ROLL]  = { .alpha = 0.95, .ang = 0.0 },
-    [CF_INST_PITCH] = { .alpha = 0.95, .ang = 0.0 },
-};
-
-///***********************************************************************************************************
 /// Global functions - definition.
 ///***********************************************************************************************************
-void cf_init(const cf_inst_t inst, const float32_t alpha, const float32_t ang)
+void cf_init(struct cf *const handle, const float32_t alpha, const float32_t ang)
 {
-    struct cf *dev = &cf_arr[inst];
+    if (handle == NULL)
+    {
+        return;
+    }
 
-    dev->alpha = alpha;
-    dev->ang   = ang;
+    handle->alpha = alpha;
+    handle->ang   = ang;
 }
 
-void cf_update(const cf_inst_t inst, const float32_t gyro_rate, const float32_t accel_ang, const float32_t dt)
+void cf_update(struct cf *const handle, const float32_t gyro_rate, const float32_t accel_ang, const float32_t dt)
 {
-    struct cf *dev = &cf_arr[inst];
-    float32_t gyro_ang = dev->ang + (gyro_rate * dt);
+    if (handle == NULL)
+    {
+        return;
+    }
 
-    dev->ang = (dev->alpha * gyro_ang) + ((1.0f - dev->alpha) * accel_ang);
+    float32_t gyro_ang = handle->ang + (gyro_rate * dt);
+
+    handle->ang = (handle->alpha * gyro_ang) + ((1.0f - handle->alpha) * accel_ang);
 }
 
-float32_t cf_ang_get(const cf_inst_t inst)
+void cf_set_ang(struct cf *const handle, const float32_t ang)
 {
-    struct cf *dev = &cf_arr[inst];
+    if (handle == NULL)
+    {
+        return;
+    }
 
-    return dev->ang;
+    handle->ang = ang;
 }
 
-void cf_ang_set(const cf_inst_t inst, const float32_t ang)
+
+void cf_set_alpha(struct cf *const handle, const float32_t alpha)
 {
-    struct cf *dev = &cf_arr[inst];
+    if (handle == NULL)
+    {
+        return;
+    }
 
-    dev->ang = ang;
-}
-
-float32_t cf_alpha_get(const cf_inst_t inst)
-{
-    struct cf *dev = &cf_arr[inst];
-
-    return dev->alpha;
-}
-
-void cf_alpha_set(const cf_inst_t inst, const float32_t alpha)
-{
-    struct cf *dev = &cf_arr[inst];
-
-    dev->alpha = alpha;
+    handle->alpha = alpha;
 }
--- modules/cf/cf.h
+++ modules/cf/cf.h
@@ -10,16 +10,55 @@ extern "C" {
 typedef float float32_t;
 
 ///
-/// \brief The complementary filter instance type.
+/// \brief The complementary filter struct.
 ///
-typedef enum cf_inst
+struct cf
 {
-    CF_INST_BEGIN = 0,
-    CF_INST_ROLL  = 0,
-    CF_INST_PITCH,
-    CF_INST_TOTAL,
-} cf_inst_t;
+    float32_t alpha;
+    float32_t ang;
+};
 
+///***********************************************************************************************************
+/// Private functions - definition.
+///***********************************************************************************************************
+///
+/// \brief Gets the angle of the complementary filter instance.
+///
+/// \param[in] handle The pointer to complementary filter.
+///
+/// \return float32_t The estimated angle.
+///
+static inline float32_t cf_get_ang(const struct cf *const handle)
+{
+    if (handle == NULL)
+    {
+        return 0.0f;
+    }
+
+    return handle->ang;
+}
+
+///
+/// \brief Gets the alpha of the complementary filter instance.
+///
+/// \param[in] handle The pointer to complementary filter.
+///
+/// \return float32_t The alpha coefficient.
+///
+static inline float32_t cf_get_alpha(const struct cf *const handle)
+{
+    if (handle == NULL)
+    {
+        return 0.0f;
+    }
+
+    return handle->alpha;
+}
+
+
+///***********************************************************************************************************
+/// Global functions - declaration.
+///***********************************************************************************************************
 ///
 /// \brief Initializes the complementary filter instance.
 ///
@@ -27,11 +66,11 @@ typedef enum cf_inst
 /// instance for angle estimation. This function should be called once before
 /// using the filter.
 ///
-/// \param[in] inst  The complementary filter instance.
-/// \param[in] alpha The initial alpha value.
-/// \param[in] ang   The initial angle value.
+/// \param[in] handle The pointer to complementary filter.
+/// \param[in] alpha  The initial alpha value.
+/// \param[in] ang    The initial angle value.
 ///
-void cf_init(const cf_inst_t inst, const float32_t alpha, const float32_t ang);
+void cf_init(struct cf *const handle, const float32_t alpha, const float32_t ang);
 
 ///
 /// \brief Updates the complementary filter with new sensor measuremets.
@@ -40,46 +79,28 @@ void cf_init(const cf_inst_t inst, const float32_t alpha, const float32_t ang);
 /// angular rate and the accelerometer-derived angle. The gyroscope data is
 /// integrated over time while the accelerometer provides long-term correction.
 ///
-/// \param[in] inst      The complementary filter instance.
+/// \param[in] handle    The pointer to complementary filter.
 /// \param[in] gyro_rate The angular rate from gyroscope.
 /// \param[in] accel_ang The angle estimated from accelerometer.
 /// \param[in] dt        The time step since the last update in seconds.
 ///
-void cf_update(const cf_inst_t inst, const float32_t gyro_rate, const float32_t accel_ang, const float32_t dt);
-
-///
-/// \brief Gets the angle of the complementary filter instance.
-///
-/// \param[in] inst The complementary filter instance.
-///
-/// \return float32_t The estimated angle.
-///
-float32_t cf_ang_get(const cf_inst_t inst);
+void cf_update(struct cf *const handle, const float32_t gyro_rate, const float32_t accel_ang, const float32_t dt);
 
 ///
 /// \brief Sets the angle of the complementary filter instance.
 ///
-/// \param[in] inst The complementary filter instance.
-/// \param[in] ang  The angle value.
+/// \param[in] handle The pointer to complementary filter.
+/// \param[in] ang    The angle value.
 ///
-void cf_ang_set(const cf_inst_t inst, const float32_t ang);
-
-///
-/// \brief Gets the alpha of the complementary filter instance.
-///
-/// \param[in] inst The complementary filter instance.
-///
-/// \return float32_t The alpha coefficient.
-///
-float32_t cf_alpha_get(const cf_inst_t inst);
+void cf_set_ang(struct cf *const handle, const float32_t ang);
 
 ///
 /// \brief Sets the alpha of the complementary filter instance.
 ///
-/// \param[in] inst  The complementary filter instance.
-/// \param[in] alpha The alpha coefficient.
+/// \param[in] handle The pointer to complementary filter.
+/// \param[in] alpha  The alpha coefficient.
 ///
-void cf_alpha_set(const cf_inst_t inst, const float32_t alpha);
+void cf_set_alpha(struct cf *const handle, const float32_t alpha);
 
 #ifdef __cplusplus
 }
