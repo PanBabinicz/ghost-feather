a45255b7bc3f0e497576578d04495d74c5efa53d
km1t4h <km1t4h@gmail.com>
2026-03-28 22:02:08 +0100

    feat: Adjust application to use pid controller

--- app/app.c
+++ app/app.c
@@ -14,13 +14,76 @@
 
 #include <math.h>
 
+struct ghf_time
+{
+    uint32_t start;
+    uint32_t stop;
+    uint32_t total;
+};
+
+struct ghf_data
+{
+    struct ghf_time time;
+    struct ahrs_raw_data ahrs;
+    float32_t roll;
+    float32_t pitch;
+    float32_t throttle;
+    float32_t yaw;
+    uint32_t  pwm1;
+    uint32_t  pwm2;
+    uint32_t  pwm3;
+    uint32_t  pwm4;
+};
+
+struct ghf_config
+{
+    float32_t kp;
+    float32_t ki;
+    float32_t kd;
+    float32_t acc_scale;
+    float32_t gyr_scale;
+    float32_t alpha;
+    float32_t dt;
+};
+
+struct ghf_module
+{
+    struct ahrs  *ahrs;
+    struct rc    *rc_1;
+    struct rc    *rc_2;
+    struct rc    *rc_3;
+    struct rc    *rc_4;
+    struct rc    *rc_5;
+    struct rc    *rc_6;
+    struct motor *motor_1;
+    struct motor *motor_2;
+    struct motor *motor_3;
+    struct motor *motor_4;
+    struct pid   *pid_roll;
+    struct pid   *pid_pitch;
+};
+
+struct ghf
+{
+    struct ghf_module module;
+    struct ghf_config config;
+    struct ghf_data   data;
+};
+
+static const float32_t max_degree = 30.0f;
+
 ///*************************************************************************************************
 /// Private functions - declaration.
 ///*************************************************************************************************
 ///
+/// \brief
+///
+static void app_setup(struct ghf *const handle);
+
+///
 /// \brief Initializes all the modules used in application.
 ///
-static void app_init(void);
+static void app_init(struct ghf *const handle);
 
 ///
 /// \brief Turns the LED on.
@@ -40,17 +103,90 @@ static void led_panic(void);
 ///*************************************************************************************************
 /// Private functions - definition.
 ///*************************************************************************************************
-static void app_init(void)
+static void app_setup(struct ghf *const handle)
 {
-    tim_init();
-    rc_init();
-    motor_init();
-    ahrs_init(ahrs_get(), 0.0f, 0.0f, 0.0f, 0.0f);
-    pid_init(pid_get(PID_INST_ROLL),  1.0f, 1.0f, 1.0f, 1.0f);
-    pid_init(pid_get(PID_INST_PITCH), 1.0f, 1.0f, 1.0f, 1.0f);
+    if (handle == NULL)
+    {
+        return;
+    }
+
+    handle->module.ahrs = ahrs_get();
+
+    handle->module.rc_1 = rc_get(RC_CH_1);
+    handle->module.rc_2 = rc_get(RC_CH_2);
+    handle->module.rc_3 = rc_get(RC_CH_3);
+    handle->module.rc_4 = rc_get(RC_CH_4);
+    handle->module.rc_5 = rc_get(RC_CH_5);
+    handle->module.rc_6 = rc_get(RC_CH_6);
+
+    handle->module.motor_1 = motor_get(MOTOR_INST_1);
+    handle->module.motor_2 = motor_get(MOTOR_INST_2);
+    handle->module.motor_3 = motor_get(MOTOR_INST_3);
+    handle->module.motor_4 = motor_get(MOTOR_INST_4);
+
+    handle->module.pid_roll  = pid_get(PID_INST_ROLL);
+    handle->module.pid_pitch = pid_get(PID_INST_PITCH);
+
+    handle->config.kp        = 0.01f;
+    handle->config.ki        = 0.00f;
+    handle->config.kd        = 0.00f;
+    handle->config.acc_scale = 1.0f / 4096.0f;
+    handle->config.gyr_scale = 1.0f / 16.4f;
+    handle->config.alpha     = 0.1f;
+    handle->config.dt        = 1.0f / 4000.0f;
+
+    handle->data.time.start = 0;
+    handle->data.time.stop  = 0;
+    handle->data.time.total = 0;
+
+    handle->data.pwm1 = 0;
+    handle->data.pwm2 = 0;
+    handle->data.pwm3 = 0;
+    handle->data.pwm4 = 0;
+
+    handle->data.roll     = 0.0f;
+    handle->data.pitch    = 0.0f;
+    handle->data.throttle = 0.0f;
+    handle->data.yaw      = 0.0f;
+
+    handle->data.ahrs.ax = 0.0f;
+    handle->data.ahrs.ay = 0.0f;
+    handle->data.ahrs.az = 0.0f;
+    handle->data.ahrs.gx = 0.0f;
+    handle->data.ahrs.gy = 0.0f;
+    handle->data.ahrs.gz = 0.0f;
+}
+
+static void app_init(struct ghf *const handle)
+{
+    if (handle == NULL)
+    {
+        return;
+    }
+
     ll_spi_dev_init(LL_SPI_INST_SPI1);
 
-    //timing_delay_us(1000 * 1000 * 15);
+    tim_init();
+
+    ahrs_init(handle->module.ahrs, handle->config.acc_scale, handle->config.gyr_scale,
+            handle->config.alpha, handle->config.dt);
+
+    rc_init(handle->module.rc_1, TIM_INST_12, LL_TIM_CCR_CH1);
+    rc_init(handle->module.rc_2, TIM_INST_12, LL_TIM_CCR_CH2);
+    rc_init(handle->module.rc_3, TIM_INST_8,  LL_TIM_CCR_CH1);
+    rc_init(handle->module.rc_4, TIM_INST_8,  LL_TIM_CCR_CH2);
+    rc_init(handle->module.rc_5, TIM_INST_8,  LL_TIM_CCR_CH3);
+    rc_init(handle->module.rc_6, TIM_INST_8,  LL_TIM_CCR_CH4);
+
+    motor_init(handle->module.motor_1, TIM_INST_4, LL_TIM_CCR_CH1);
+    motor_init(handle->module.motor_2, TIM_INST_4, LL_TIM_CCR_CH2);
+    motor_init(handle->module.motor_3, TIM_INST_4, LL_TIM_CCR_CH3);
+    motor_init(handle->module.motor_4, TIM_INST_4, LL_TIM_CCR_CH4);
+
+    pid_init(handle->module.pid_roll,  handle->config.kp, handle->config.ki, handle->config.kd, handle->config.dt);
+    pid_init(handle->module.pid_pitch, handle->config.kp, handle->config.ki, handle->config.kd, handle->config.dt);
+
+    timing_delay_us(1000 * 1000 * 5);
 
     if (bmi270_init() != BMI270_RES_OK)
     {
@@ -79,90 +215,91 @@ static void led_panic(void)
 ///*************************************************************************************************
 void app_start(void)
 {
-    struct rc_dev *rc_dev_arr = rc_dev_arr_get();
-    struct rc_dev *rc_dev_3 = &rc_dev_arr[RC_CH_3];
+    struct ghf ghf;
 
-    uint32_t dwt_cnt_start, dwt_cnt_stop, dwt_cnt_total;
+    float32_t time_in_us;
+    float32_t time;
 
-    app_init();
+    app_setup(&ghf);
+    app_init(&ghf);
 
     /* Never return */
     while (1)
     {
-        dwt_cnt_start = timing_cnt_get();
+        ghf.data.time.start = timing_cnt_get();
         vtol_take_off_proc();
-
-        bmi270_acc_read();
-        bmi270_gyr_read();
-
-        //ahrs_update(bmi270_acc_get_x(), bmi270_acc_get_y(), bmi270_acc_get_z());
 
         if (vtol_stat_get() == VTOL_STAT_ON)
         {
+            bmi270_acc_read();
+            bmi270_gyr_read();
 
-            struct rc_sig *roll     = &rc_dev_arr[RC_CH_4].sig;
-            struct rc_sig *pitch    = &rc_dev_arr[RC_CH_2].sig;
-            struct rc_sig *throttle = &rc_dev_arr[RC_CH_3].sig;
-            struct rc_sig *yaw      = &rc_dev_arr[RC_CH_1].sig;
+            ghf.data.ahrs.ax = bmi270_acc_get_x();
+            ghf.data.ahrs.ay = bmi270_acc_get_y();
+            ghf.data.ahrs.az = bmi270_acc_get_z();
+            ghf.data.ahrs.gx = bmi270_gyr_get_x();
+            ghf.data.ahrs.gy = bmi270_gyr_get_y();
+            ghf.data.ahrs.gz = bmi270_gyr_get_z();
 
-            int32_t pwm1, pwm2, pwm3, pwm4;
+            ahrs_update(ghf.module.ahrs, &ghf.data.ahrs);
 
-            rc_sig_raw_gen(RC_CH_1);
-            rc_sig_raw_gen(RC_CH_2);
-            rc_sig_raw_gen(RC_CH_3);
-            rc_sig_raw_gen(RC_CH_4);
+            rc_sig_raw_gen(ghf.module.rc_1);
+            rc_sig_raw_gen(ghf.module.rc_2);
+            rc_sig_raw_gen(ghf.module.rc_3);
+            rc_sig_raw_gen(ghf.module.rc_4);
 
-            rc_sig_norm(RC_CH_1);
-            rc_sig_norm(RC_CH_2);
-            rc_sig_norm(RC_CH_3);
-            rc_sig_norm(RC_CH_4);
+            rc_sig_norm(ghf.module.rc_1, RC_NORM_SYM);
+            rc_sig_norm(ghf.module.rc_2, RC_NORM_SYM);
+            rc_sig_norm(ghf.module.rc_3, RC_NORM_ASYM);
+            rc_sig_norm(ghf.module.rc_4, RC_NORM_SYM);
 
-            throttle->norm = (throttle->norm > 1.0) ? 1.0 : throttle->norm;
+            ghf.data.throttle = ghf.module.rc_3->sig.norm;
 
-            if (throttle->norm > 0.3)
+            ghf.data.roll  = pid_update(ghf.module.pid_roll,  ghf.module.rc_1->sig.norm*max_degree, ghf.module.ahrs->out.roll);
+            ghf.data.pitch = pid_update(ghf.module.pid_pitch, ghf.module.rc_2->sig.norm*max_degree, ghf.module.ahrs->out.roll);
+            ghf.data.yaw   = ghf.module.rc_4->sig.norm * 0.33f;
+
+            if (ghf.data.throttle < 0.2f)
             {
-                roll->norm = (roll->norm >  1.0) ?  1.0 : roll->norm;
-                roll->norm = (roll->norm < -1.0) ? -1.0 : roll->norm;
-
-                pitch->norm = (pitch->norm >  1.0) ?  1.0 : pitch->norm;
-                pitch->norm = (pitch->norm < -1.0) ? -1.0 : pitch->norm;
-
-                yaw->norm = (yaw->norm >  1.0) ?  1.0 : yaw->norm;
-                yaw->norm = (yaw->norm < -1.0) ? -1.0 : yaw->norm;
-            }
-            else
-            {
-                roll->norm  = 0.0;
-                pitch->norm = 0.0;
-                yaw->norm   = 0.0;
+                ghf.data.roll  = 0.0f;
+                ghf.data.pitch = 0.0f;
+                ghf.data.yaw   = 0.0f;
             }
 
-            pwm1 = 1000 + (1000 * (throttle->norm - roll->norm + pitch->norm + yaw->norm));
-            pwm2 = 1000 + (1000 * (throttle->norm + roll->norm + pitch->norm - yaw->norm));
-            pwm3 = 1000 + (1000 * (throttle->norm + roll->norm - pitch->norm + yaw->norm));
-            pwm4 = 1000 + (1000 * (throttle->norm - roll->norm - pitch->norm - yaw->norm));
+            ghf.data.pwm1 = 1000 + (1000 * (ghf.data.throttle + ghf.data.roll + ghf.data.pitch + ghf.data.yaw));
+            ghf.data.pwm2 = 1000 + (1000 * (ghf.data.throttle - ghf.data.roll + ghf.data.pitch - ghf.data.yaw));
+            ghf.data.pwm3 = 1000 + (1000 * (ghf.data.throttle + ghf.data.roll - ghf.data.pitch - ghf.data.yaw));
+            ghf.data.pwm4 = 1000 + (1000 * (ghf.data.throttle - ghf.data.roll - ghf.data.pitch + ghf.data.yaw));
 
-            pwm1 = (pwm1 > 2000) ? 2000 : pwm1;
-            pwm1 = (pwm1 < 1000) ? 1000 : pwm1;
+            ghf.data.pwm1 = (ghf.data.pwm1 > 2000) ? 2000 : ghf.data.pwm1;
+            ghf.data.pwm1 = (ghf.data.pwm1 < 1000) ? 1000 : ghf.data.pwm1;
 
-            pwm2 = (pwm2 > 2000) ? 2000 : pwm2;
-            pwm2 = (pwm2 < 1000) ? 1000 : pwm2;
+            ghf.data.pwm2 = (ghf.data.pwm2 > 2000) ? 2000 : ghf.data.pwm2;
+            ghf.data.pwm2 = (ghf.data.pwm2 < 1000) ? 1000 : ghf.data.pwm2;
 
-            pwm3 = (pwm3 > 2000) ? 2000 : pwm3;
-            pwm3 = (pwm3 < 1000) ? 1000 : pwm3;
+            ghf.data.pwm3 = (ghf.data.pwm3 > 2000) ? 2000 : ghf.data.pwm3;
+            ghf.data.pwm3 = (ghf.data.pwm3 < 1000) ? 1000 : ghf.data.pwm3;
 
-            pwm4 = (pwm4 > 2000) ? 2000 : pwm4;
-            pwm4 = (pwm4 < 1000) ? 1000 : pwm4;
+            ghf.data.pwm4 = (ghf.data.pwm4 > 2000) ? 2000 : ghf.data.pwm4;
+            ghf.data.pwm4 = (ghf.data.pwm4 < 1000) ? 1000 : ghf.data.pwm4;
 
-            motor_upd(MOTOR_1, pwm1);
-            motor_upd(MOTOR_2, pwm2);
-            motor_upd(MOTOR_3, pwm3);
-            motor_upd(MOTOR_4, pwm4);
+            motor_update(ghf.module.motor_1, ghf.data.pwm1);
+            motor_update(ghf.module.motor_2, ghf.data.pwm2);
+            motor_update(ghf.module.motor_3, ghf.data.pwm3);
+            motor_update(ghf.module.motor_4, ghf.data.pwm4);
         }
 
         vtol_land_proc();
 
-        dwt_cnt_stop = timing_cnt_get();
-        dwt_cnt_total = dwt_cnt_stop - dwt_cnt_start;
+        //time = (timing_cnt_get() - ghf.data.time.start) * TIMING_TICK_DURATION * 1000000;
+
+        do
+        {
+            ghf.data.time.stop  = timing_cnt_get();
+            ghf.data.time.total = ghf.data.time.stop - ghf.data.time.start;
+            ghf.data.time.total = ghf.data.time.total * TIMING_TICK_DURATION * 1000000;
+        } while (ghf.data.time.total < ghf.config.dt * 1000000);
+
+        time = ghf.data.time.total;
     }
 }
