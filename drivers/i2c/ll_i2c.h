607817d21d2658b8fdf3678fca923a042102b0dd
km1t4h <km1t4h@gmail.com>
2026-05-05 23:36:34 +0200

    feat: software: wip: Add I2C low level driver #1

---
+++ drivers/i2c/ll_i2c.h
@@ -0,0 +1,16 @@
+#ifndef _LL_I2C_H
+#define _LL_I2C_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif  /* __cplusplus */
+
+
+#ifdef __cplusplus
+}
+#endif  /* __cplusplus */
+
+#endif  /* _LL_I2C_H */
+
