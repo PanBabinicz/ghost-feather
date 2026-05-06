c376c796d08abfa51da326d785bc4785309001de
km1t4h <km1t4h@gmail.com>
2026-05-06 23:55:48 +0200

    feat: software: wip: Add I2C low level driver #2

--- drivers/i2c/ll_i2c.h
+++ drivers/i2c/ll_i2c.h
@@ -7,6 +7,26 @@
 extern "C" {
 #endif  /* __cplusplus */
 
+///
+/// \brief The I2C result type.
+///
+typedef enum ll_i2c_res
+{
+    LL_I2C_RES_OK = 0,
+    LL_I2C_RES_ERR,
+} ll_i2c_res_t;
+
+///
+/// \brief The I2C status type.
+///
+/// This enum reflects whether a I2C peripheral is currently initialized or
+/// not.
+///
+typedef enum ll_i2c_stat
+{
+    LL_I2C_STAT_DEINIT = 0,
+    LL_I2C_STAT_INIT,
+} ll_i2c_stat_t;
 
 #ifdef __cplusplus
 }
