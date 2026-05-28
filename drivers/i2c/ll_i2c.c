cac9f82ca0a8a62c4f9fa4460645a39076351475
km1t4h <km1t4h@gmail.com>
2026-05-28 23:33:19 +0200

    feat: software: wip: Add I2C low level driver #40. Adds start function

--- drivers/i2c/ll_i2c.c
+++ drivers/i2c/ll_i2c.c
@@ -413,3 +413,15 @@ ll_i2c_res_t ll_i2c_set_xfer_dir_read(const uint32_t address)
 
     return LL_I2C_RES_OK;
 }
+
+ll_i2c_res_t ll_i2c_start(const uint32_t address)
+{
+    if ((LL_I2C_REG_CR2(address) & LL_I2C_CR2_START_MASK) == 0x01)
+    {
+        return LL_I2C_RES_ERR;
+    }
+
+    LL_I2C_REG_CR2(address) |= LL_I2C_CR2_START_MASK;
+
+    return LL_I2C_RES_OK;
+}
--- drivers/i2c/ll_i2c.h
+++ drivers/i2c/ll_i2c.h
@@ -866,6 +866,17 @@ ll_i2c_res_t ll_i2c_set_xfer_dir_write(const uint32_t address);
 ///
 ll_i2c_res_t ll_i2c_set_xfer_dir_read(const uint32_t address);
 
+///
+/// \brief Generates the I2C start condition.
+///
+/// \param[in] address The i2c address.
+///
+/// \return ll_i2c_res_t   The I2C result.
+/// \retval LL_I2C_RES_OK  On success.
+/// \retval LL_I2C_RES_ERR Otherwise.
+///
+ll_i2c_res_t ll_i2c_start(const uint32_t address);
+
 #ifdef __cplusplus
 }
 #endif  /* __cplusplus */
